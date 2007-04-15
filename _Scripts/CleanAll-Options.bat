b8f508cd4b0414811afbb3a939cc295d2e15b4b7
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2007-04-15 15:41:14 +0000

    Moved to /trunk

--- _Scripts/CleanAll-Options.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-PUSHD ..
-
-ECHO Cleaning user options...
-DEL /F /Q /A:H "*.suo"
-ECHO ...successfully cleaned
-
-POPD
--- _Scripts/CleanAll.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-CALL CleanAll-Options.bat
-CALL CleanAll-Setup.bat
-CALL CleanAll-Temp.bat
