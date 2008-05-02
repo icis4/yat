9ef33f559f9e893c30a83e7e2b1b7f958a023f74
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2008-05-02 14:33:43 +0000

    Name of non-YAT assembly projects adapted to assembly name "MKY.*"

--- trunk/MKY.Net.Sockets/Properties/AssemblyInfo.cs
+++ trunk/MKY.Net.Sockets/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MKY.Net.Sockets")]
-[assembly: AssemblyDescription("Net.Sockets extension")]
+[assembly: AssemblyDescription("System.Net.Sockets extension")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MKY")]
 [assembly: AssemblyProduct("MKY.Net.Sockets")]
