7e45b8a851852ad1d7c75b0bb85a4f641d023db9
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2008-05-19 20:51:04 +0000

    Domain.IO => MKY.IO.Serial (2nd try)

---
+++ MKY.IO.Serial/SerialPort/FlowControl.cs
@@ -0,0 +1,189 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MKY.Utilities.Types;
+
+namespace YAT.Domain.IO
+{
+	#region Enum FlowControl
+
+	/// <summary></summary>
+	/// <remarks>
+	/// I think flow control is the better identifier, no clue why .NET uses the
+	/// term flowControl.
+	/// </remarks>
+	public enum FlowControl
+	{
+		/// <summary></summary>
+		None = System.IO.Ports.Handshake.None,
+		/// <summary></summary>
+		RequestToSend = System.IO.Ports.Handshake.RequestToSend,
+		/// <summary></summary>
+		XOnXOff = System.IO.Ports.Handshake.XOnXOff,
+		/// <summary></summary>
+		RequestToSendXOnXOff = System.IO.Ports.Handshake.RequestToSendXOnXOff,
+		/// <summary></summary>
+		Manual,
+		/// <summary></summary>
+		RS485,
+	}
+
+	#endregion
+
+	/// <summary></summary>
+	[Serializable]
+	public class XFlowControl : MKY.IO.Ports.XHandshake
+	{
+		#region String Definitions
+
+		private const string Manual_string = "Manual";
+		private const string Manual_stringShort = "Manual";
+		private const string RS485_string = "RS-485 Transceiver Control";
+		private const string RS485_stringShort = "RS-485";
+
+		#endregion
+
+		/// <summary>Default is <see cref="FlowControl.None"/></summary>
+		public XFlowControl()
+			: base((System.IO.Ports.Handshake)FlowControl.None)
+		{
+		}
+
+		/// <summary></summary>
+		protected XFlowControl(FlowControl flowControl)
+			: base((System.IO.Ports.Handshake)flowControl)
+		{
+		}
+
+		#region ToString
+
+		/// <summary></summary>
+		public override string ToString()
+		{
+			switch ((FlowControl)UnderlyingEnum)
+			{
+				case FlowControl.Manual: return (Manual_string);
+				case FlowControl.RS485:  return (RS485_string);
+				default:                 return (base.ToString());
+			}
+		}
+
+		/// <summary></summary>
+		public new string ToShortString()
+		{
+			switch ((FlowControl)UnderlyingEnum)
+			{
+				case FlowControl.Manual: return (Manual_stringShort);
+				case FlowControl.RS485:  return (RS485_stringShort);
+				default:                 return (base.ToShortString());
+			}
+		}
+
+		#endregion
+
+		#region GetItems
+
+		/// <summary></summary>
+		public new static XFlowControl[] GetItems()
+		{
+			List<XFlowControl> a = new List<XFlowControl>();
+			a.Add(new XFlowControl(FlowControl.None));
+			a.Add(new XFlowControl(FlowControl.RequestToSend));
+			a.Add(new XFlowControl(FlowControl.XOnXOff));
+			a.Add(new XFlowControl(FlowControl.RequestToSendXOnXOff));
+			a.Add(new XFlowControl(FlowControl.Manual));
+			a.Add(new XFlowControl(FlowControl.RS485));
+			return (a.ToArray());
+		}
+
+		#endregion
+
+		#region Parse
+
+		/// <summary></summary>
+		public new static XFlowControl Parse(string flowControl)
+		{
+			if      ((string.Compare(flowControl, Manual_string, true) == 0) ||
+					 (string.Compare(flowControl, Manual_stringShort, true) == 0))
+			{
+				return (new XFlowControl(FlowControl.Manual));
+			}
+			else if ((string.Compare(flowControl, RS485_string, true) == 0) ||
+					 (string.Compare(flowControl, RS485_stringShort, true) == 0))
+			{
+				return (new XFlowControl(FlowControl.RS485));
+			}
+			else
+			{
+				return ((XFlowControl)MKY.IO.Ports.XHandshake.Parse(flowControl));
+			}
+		}
+
+		#endregion
+
+		#region Conversion Operators
+
+		/// <summary></summary>
+		public static implicit operator FlowControl(XFlowControl flowControl)
+		{
+			return ((FlowControl)flowControl.UnderlyingEnum);
+		}
+
+		/// <summary></summary>
+		public static implicit operator XFlowControl(FlowControl flowControl)
+		{
+			return (new XFlowControl(flowControl));
+		}
+
+		/// <summary></summary>
+		public static implicit operator int(XFlowControl flowControl)
+		{
+			return (flowControl.GetHashCode());
+		}
+
+		/// <summary></summary>
+		public static implicit operator XFlowControl(int flowControl)
+		{
+			return (new XFlowControl((FlowControl)flowControl));
+		}
+
+		/// <summary></summary>
+		public static implicit operator string(XFlowControl flowControl)
+		{
+			return (flowControl.ToString());
+		}
+
+		/// <summary></summary>
+		public static implicit operator XFlowControl(string flowControl)
+		{
+			return (Parse(flowControl));
+		}
+
+		/// <summary></summary>
+		public static implicit operator System.IO.Ports.Handshake(XFlowControl flowControl)
+		{
+			switch ((FlowControl)flowControl.UnderlyingEnum)
+			{
+				case FlowControl.None:
+				case FlowControl.RequestToSend:
+				case FlowControl.XOnXOff:
+				case FlowControl.RequestToSendXOnXOff:
+					return ((System.IO.Ports.Handshake)(FlowControl)flowControl);
+
+				case FlowControl.RS485:
+				case FlowControl.Manual:
+				default:
+					return (System.IO.Ports.Handshake.None);
+			}
+		}
+
+		/// <summary></summary>
+		public static implicit operator XFlowControl(System.IO.Ports.Handshake flowControl)
+		{
+			return (new XFlowControl((FlowControl)flowControl));
+		}
+
+		#endregion
+	}
+}
---
+++ MKY.IO.Serial/Socket/TcpClient.cs
@@ -0,0 +1,545 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+using MKY.Utilities.Event;
+
+namespace YAT.Domain.IO
+{
+	/// <summary></summary>
+	public class TcpClient : IIOProvider, IDisposable, MKY.Net.Sockets.ISocketService
+	{
+		#region Types
+		//==========================================================================================
+		// Types
+		//==========================================================================================
+
+		private enum SocketState
+		{
+			Disconnecting,
+			Disconnected,
+			Connecting,
+			Connected,
+			WaitingForReconnect,
+			Error,
+		}
+
+		#endregion
+
+		#region Fields
+		//==========================================================================================
+		// Fields
+		//==========================================================================================
+
+		private bool _isDisposed;
+
+		private System.Net.IPAddress _remoteIPAddress;
+		private int _remotePort;
+		private System.Net.IPAddress _localIPAddress;
+		private AutoRetry _autoReconnect;
+
+		private SocketState _state = SocketState.Disconnected;
+		private object _stateSyncObj = new object();
+
+		private Queue<byte> _receiveBuffer = new Queue<byte>();
+
+		private MKY.Net.Sockets.SocketClient _socket;
+		private MKY.Net.Sockets.ISocketConnection _socketConnection;
+		private object _socketConnectionSyncObj = new object();
+
+		private System.Timers.Timer _reconnectTimer;
+
+		#endregion
+
+		#region Events
+		//==========================================================================================
+		// Events
+		//==========================================================================================
+
+		/// <summary></summary>
+		public event EventHandler IOChanged;
+		/// <summary></summary>
+		public event EventHandler IOControlChanged;
+		/// <summary></summary>
+		public event EventHandler<IOErrorEventArgs> IOError;
+		/// <summary></summary>
+		public event EventHandler DataReceived;
+		/// <summary></summary>
+		public event EventHandler DataSent;
+
+		#endregion
+
+		#region Object Lifetime
+		//==========================================================================================
+		// Object Lifetime
+		//==========================================================================================
+
+		/// <summary></summary>
+		public TcpClient(System.Net.IPAddress remoteIPAddress, int remotePort, System.Net.IPAddress localIPAddress)
+		{
+			_remoteIPAddress = remoteIPAddress;
+			_remotePort = remotePort;
+			_localIPAddress = localIPAddress;
+			_autoReconnect = new AutoRetry();
+		}
+
+		/// <summary></summary>
+		public TcpClient(System.Net.IPAddress remoteIPAddress, int remotePort, System.Net.IPAddress localIPAddress, AutoRetry autoReconnect)
+		{
+			_remoteIPAddress = remoteIPAddress;
+			_remotePort = remotePort;
+			_localIPAddress = localIPAddress;
+			_autoReconnect = autoReconnect;
+		}
+
+		#region Disposal
+		//------------------------------------------------------------------------------------------
+		// Disposal
+		//------------------------------------------------------------------------------------------
+
+		/// <summary></summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary></summary>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_isDisposed)
+			{
+				if (disposing)
+				{
+					StopAndDisposeReconnectTimer();
+					DisposeSocket();
+				}
+				_isDisposed = true;
+			}
+		}
+
+		/// <summary></summary>
+		~TcpClient()
+		{
+			Dispose(false);
+		}
+
+		/// <summary></summary>
+		protected bool IsDisposed
+		{
+			get { return (_isDisposed); }
+		}
+
+		/// <summary></summary>
+		protected void AssertNotDisposed()
+		{
+			if (_isDisposed)
+				throw (new ObjectDisposedException(GetType().ToString(), "Object has already been disposed"));
+		}
+
+		#endregion
+
+		#endregion
+
+		#region Properties
+		//==========================================================================================
+		// Properties
+		//==========================================================================================
+
+		/// <summary></summary>
+		public bool HasStarted
+		{
+			get
+			{
+				AssertNotDisposed();
+				switch (_state)
+				{
+					case SocketState.Connecting:
+					case SocketState.Connected:
+					case SocketState.WaitingForReconnect:
+					{
+						return (true);
+					}
+					default:
+					{
+						return (false);
+					}
+				}
+			}
+		}
+
+		private bool IsConnectedOrConnecting
+		{
+			get
+			{
+				AssertNotDisposed();
+				switch (_state)
+				{
+					case SocketState.Connecting:
+					case SocketState.Connected:
+					{
+						return (true);
+					}
+					default:
+					{
+						return (false);
+					}
+				}
+			}
+		}
+
+		/// <summary></summary>
+		public bool IsConnected
+		{
+			get
+			{
+				AssertNotDisposed();
+				switch (_state)
+				{
+					case SocketState.Connected:
+					{
+						return (true);
+					}
+					default:
+					{
+						return (false);
+					}
+				}
+			}
+		}
+
+		/// <summary></summary>
+		public int BytesAvailable
+		{
+			get
+			{
+				AssertNotDisposed();
+				return (_receiveBuffer.Count);
+			}
+		}
+
+		/// <summary></summary>
+		public object UnderlyingIOInstance
+		{
+			get
+			{
+				AssertNotDisposed();
+				return (_socket);
+			}
+		}
+
+		#endregion
+
+		#region Public Methods
+		//==========================================================================================
+		// Public Methods
+		//==========================================================================================
+
+		/// <summary></summary>
+		public void Start()
+		{
+			AssertNotDisposed();
+
+			if (!HasStarted)
+				StartSocket();
+		}
+
+		/// <summary></summary>
+		public void Stop()
+		{
+			AssertNotDisposed();
+
+			if (HasStarted)
+				StopSocket();
+		}
+
+		/// <summary></summary>
+		public int Receive(out byte[] buffer)
+		{
+			AssertNotDisposed();
+		
+			if (_receiveBuffer.Count > 0)
+			{
+				lock (_receiveBuffer)
+				{
+					int count = _receiveBuffer.Count;
+					buffer = new byte[count];
+					for (int i = 0; i < count; i++)
+						buffer[i] = _receiveBuffer.Dequeue();
+				}
+			}
+			else
+			{
+				buffer = new byte[] { };
+			}
+			return (buffer.Length);
+		}
+
+		/// <summary></summary>
+		public void Send(byte[] buffer)
+		{
+			AssertNotDisposed();
+
+			if (HasStarted)
+			{
+				if (_socketConnection != null)
+					_socketConnection.BeginSend(buffer);
+			}
+		}
+
+		#endregion
+
+		#region Simple Socket Methods
+		//==========================================================================================
+		// Simple Socket Methods
+		//==========================================================================================
+
+		private void DisposeSocket()
+		{
+			if (_socket != null)
+			{
+				_socket.Dispose();
+				_socket = null;
+				_socketConnection = null;
+			}
+		}
+
+		#endregion
+
+		#region Socket Methods
+		//==========================================================================================
+		// Socket Methods
+		//==========================================================================================
+
+		private void StartSocket()
+		{
+			_socket = new MKY.Net.Sockets.SocketClient((MKY.Net.Sockets.ISocketService)this, null, 2048, 8192, 0, 0, Timeout.Infinite, Timeout.Infinite);
+			_socket.OnException += new EventHandler<MKY.Net.Sockets.ExceptionEventArgs>(_socket_OnException);
+			_socket.AddConnector(new System.Net.IPEndPoint(_remoteIPAddress, _remotePort));
+			_socket.Start();
+			
+			lock (_stateSyncObj)
+				_state = SocketState.Connecting;
+			
+			OnIOChanged(new EventArgs());
+		}
+
+		private void StopSocket()
+		{
+			lock (_stateSyncObj)
+				_state = SocketState.Disconnecting;
+
+			_socket.Stop();
+			DisposeSocket();
+
+			OnIOChanged(new EventArgs());
+		}
+
+		private void RestartSocket()
+		{
+			Stop();
+			Start();
+		}
+
+		#endregion
+
+		#region Socket Events
+		//==========================================================================================
+		// Socket Events
+		//==========================================================================================
+
+		private void _socket_OnException(object sender, MKY.Net.Sockets.ExceptionEventArgs e)
+		{
+			if (_autoReconnect.Enabled)
+			{
+				lock (_stateSyncObj)
+					_state = SocketState.WaitingForReconnect;
+			
+				OnIOChanged(new EventArgs());
+
+				StartReconnectTimer();
+			}
+			else
+			{
+				DisposeSocket();
+
+				lock (_stateSyncObj)
+					_state = SocketState.Error;
+				
+				OnIOChanged(new EventArgs());
+				OnIOError(new IOErrorEventArgs(e.Exception.Message));
+			}
+		}
+
+		#endregion
+
+		#region ISocketService Members
+		//==========================================================================================
+		// ISocketService Members
+		//==========================================================================================
+
+		/// <summary></summary>
+		public void OnConnected(MKY.Net.Sockets.ConnectionEventArgs e)
+		{
+			lock (_socketConnectionSyncObj)
+				_socketConnection = e.Connection;
+			
+			lock (_stateSyncObj)
+				_state = SocketState.Connected;
+
+			OnIOChanged(new EventArgs());
+
+			// immediately begin receiving data
+			e.Connection.BeginReceive();
+		}
+
+		/// <summary></summary>
+		public void OnReceived(MKY.Net.Sockets.MessageEventArgs e)
+		{
+			lock (_receiveBuffer)
+			{
+				foreach (byte b in e.Buffer)
+					_receiveBuffer.Enqueue(b);
+			}
+			OnDataReceived(new EventArgs());
+
+			// continue receiving data
+			e.Connection.BeginReceive();
+		}
+
+		/// <summary></summary>
+		public void OnSent(MKY.Net.Sockets.MessageEventArgs e)
+		{
+			// nothing to do
+		}
+
+		/// <summary></summary>
+		public void OnDisconnected(MKY.Net.Sockets.DisconnectedEventArgs e)
+		{
+			if (e.Exception == null)
+			{
+				// normal disconnect
+				lock (_socketConnectionSyncObj)
+					_socketConnection = null;
+
+				if (_autoReconnect.Enabled)
+				{
+					lock (_stateSyncObj)
+						_state = SocketState.WaitingForReconnect;
+
+					OnIOChanged(new EventArgs());
+
+					StartReconnectTimer();
+				}
+				else
+				{
+					lock (_stateSyncObj)
+						_state = SocketState.Disconnected;
+
+					OnIOChanged(new EventArgs());
+				}
+			}
+			else
+			{
+				// exception disconnect
+				DisposeSocket();
+
+				lock (_socketConnectionSyncObj)
+					_socketConnection = null;
+
+				lock (_stateSyncObj)
+					_state = SocketState.Error;
+
+				OnIOChanged(new EventArgs());
+				OnIOError(new IOErrorEventArgs(e.Exception.Message));
+			}
+		}
+
+		#endregion
+
+		#region Reconnect Timer
+		//==========================================================================================
+		// Reconnect Timer
+		//==========================================================================================
+
+		private void StartReconnectTimer()
+		{
+			if (_reconnectTimer != null)
+				StopAndDisposeReconnectTimer();
+
+			_reconnectTimer = new System.Timers.Timer(_autoReconnect.Interval);
+			_reconnectTimer.AutoReset = false;
+			_reconnectTimer.Elapsed += new System.Timers.ElapsedEventHandler(_reconnectTimer_Elapsed);
+			_reconnectTimer.Start();
+		}
+
+		private void StopAndDisposeReconnectTimer()
+		{
+			if (_reconnectTimer != null)
+			{
+				_reconnectTimer.Stop();
+				_reconnectTimer.Dispose();
+				_reconnectTimer = null;
+			}
+		}
+
+		private void _reconnectTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+		{
+			if (!IsDisposed && HasStarted && !IsConnectedOrConnecting)
+			{
+				try
+				{
+					StartSocket();
+				}
+				catch
+				{
+					StartReconnectTimer();
+				}
+			}
+			else
+			{
+				StopAndDisposeReconnectTimer();
+			}
+		}
+
+		#endregion
+
+		#region Event Invoking
+		//==========================================================================================
+		// Event Invoking
+		//==========================================================================================
+
+		/// <summary></summary>
+		protected virtual void OnIOChanged(EventArgs e)
+		{
+			EventHelper.FireSync(IOChanged, this, e);
+		}
+
+		/// <summary></summary>
+		protected virtual void OnIOControlChanged(EventArgs e)
+		{
+			EventHelper.FireSync(IOControlChanged, this, e);
+		}
+
+		/// <summary></summary>
+		protected virtual void OnIOError(IOErrorEventArgs e)
+		{
+			EventHelper.FireSync<IOErrorEventArgs>(IOError, this, e);
+		}
+
+		/// <summary></summary>
+		protected virtual void OnDataReceived(EventArgs e)
+		{
+			EventHelper.FireSync(DataReceived, this, e);
+		}
+
+		/// <summary></summary>
+		protected virtual void OnDataSent(EventArgs e)
+		{
+			EventHelper.FireSync(DataSent, this, e);
+		}
+
+		#endregion
+	}
+}
