0f62b932b728edc558cff5ee2885aceb3adb301a
uese69 <uese69@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2009-01-27 15:18:30 +0000

    ToString(): has to display the hours, if hours or days are bigger than 0.

--- trunk/MKY.Windows.Forms/Chronometer.cs
+++ trunk/MKY.Windows.Forms/Chronometer.cs
@@ -143,7 +143,7 @@ namespace MKY.Windows.Forms
 			sb.Insert(0, ts.Seconds.ToString("D2"));
 			sb.Insert(0, ":");
 			sb.Insert(0, ts.Minutes.ToString());
-			if (ts.Hours > 0)
+			if ((ts.Hours > 0) | (ts.Days > 0))
 			{
 				sb.Insert(0, ":");
 				sb.Insert(0, ts.Hours.ToString());
