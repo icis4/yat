0f427ae2374af0182fc73fc21edf842d678fac32
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2009-09-08 20:53:48 +0000

    Versions 1.99.22 => 1.99.23

--- trunk/YAT.Settings.Test/Properties/AssemblyInfo.cs
+++ trunk/YAT.Settings.Test/Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.99.*")]
-[assembly: AssemblyFileVersion("1.99.22.0")]
+[assembly: AssemblyFileVersion("1.99.23.0")]
 
 // CLS compliance
 [assembly: CLSCompliant(true)]
