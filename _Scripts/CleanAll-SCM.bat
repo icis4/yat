d9081a7f05b7bc3474e0c87d7d7291563db405ce
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2010-03-04 23:50:56 +0000

    libusb.NET added

--- _Scripts/CleanAll-SCM.bat
+++ _Scripts/CleanAll-SCM.bat
@@ -6,6 +6,10 @@ ECHO Cleaning all \.svn directories...
 FOR /R %%I IN (.) DO CALL "_Scripts\CleanDir.bat" "%%I" ".svn"
 ECHO ...successfully cleaned
 
+ECHO Cleaning all \.git directories...
+FOR /R %%I IN (.) DO CALL "_Scripts\CleanDir.bat" "%%I" ".git"
+ECHO ...successfully cleaned
+
 ECHO Cleaning Ankh.Load...
 DEL /F /Q /A:H "Ankh.Load"
 ECHO ...successfully cleaned
---
+++ libusb.NET.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,62 @@
+﻿//==================================================================================================
+// $URL: https://y-a-terminal.svn.sourceforge.net/svnroot/y-a-terminal/trunk/MKY.IO.Ports/Properties/AssemblyInfo.cs $
+// $Author: maettu_this $
+// $Date: 2010-02-23 00:18:29 +0100 (Di, 23 Feb 2010) $
+// $Revision: 254 $
+// ------------------------------------------------------------------------------------------------
+// See SVN change log for revision details.
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2004 Mike Krueger. 
+// Copyright © 2010 Matthias Kläy.
+// All rights reserved.
+// ------------------------------------------------------------------------------------------------
+// This source code is licensed under the GNU LGPL.
+// See http://www.gnu.org/licenses/lgpl.html for license details.
+//==================================================================================================
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("libusb.NET.Test")]
+[assembly: AssemblyDescription("libusb wrapper for .NET test")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("libusb")]
+[assembly: AssemblyProduct("libusb.NET.Test")]
+[assembly: AssemblyCopyright("Copyright © 2004 Mike Krueger. Copyright © 2010 Matthias Kläy. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be08a40c-d851-4028-ba16-f1e32acfccf3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
+// CLS compliance
+[assembly: CLSCompliant(true)]
+
+//==================================================================================================
+// End of
+// $URL: https://y-a-terminal.svn.sourceforge.net/svnroot/y-a-terminal/trunk/MKY.IO.Ports/Properties/AssemblyInfo.cs $
+//==================================================================================================
