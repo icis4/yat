4fa28bc2985ea50ed4993e398f8767aabf20789c
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2011-08-09 11:43:15 +0000

    MKY.IO.Serial split into IO and IO.Net to ensure that TCP/IP and UDP/IP functionality can be used independent from IO.Ports and IO.Usb.

---
+++ MKY/MKY.IO.Net/TcpServer.cs
@@ -0,0 +1,611 @@
+﻿//==================================================================================================
+// YAT - Yet Another Terminal.
+// Visit YAT at http://sourceforge.net/projects/y-a-terminal.
+// Contact YAT by mailto:y-a-terminal@users.sourceforge.net.
+// ------------------------------------------------------------------------------------------------
+// $URL: https://y-a-terminal.svn.sourceforge.net/svnroot/y-a-terminal/trunk/MKY/MKY.IO.Serial/Socket/TcpServer.cs $
+// $Author: maettu_this $
+// $Date: 2011-04-25 13:56:30 +0200 (Mo, 25 Apr 2011) $
+// $Revision: 416 $
+// ------------------------------------------------------------------------------------------------
+// See SVN change log for revision details.
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2003-2004 HSR Hochschule für Technik Rapperswil.
+// Copyright © 2003-2011 Matthias Kläy.
+// All rights reserved.
+// ------------------------------------------------------------------------------------------------
+// This source code is licensed under the GNU LGPL.
+// See http://www.gnu.org/licenses/lgpl.html for license details.
+//==================================================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Threading;
+
+using MKY.Event;
+
+namespace MKY.IO.Net
+{
+	/// <summary></summary>
+	public class TcpServer : IIOProvider, IDisposable, ALAZ.SystemEx.NetEx.SocketsEx.ISocketService
+	{
+		#region Types
+		//==========================================================================================
+		// Types
+		//==========================================================================================
+
+		private enum SocketState
+		{
+			Reset,
+			Listening,
+			Accepted,
+			Stopping,
+			Error,
+		}
+
+		#endregion
+
+		#region Delegates
+		//==========================================================================================
+		// Delegates
+		//==========================================================================================
+
+		/// <summary></summary>
+		public delegate void AsyncStopSocketCaller();
+
+		#endregion
+
+		#region Static Fields
+		//==========================================================================================
+		// Static Fields
+		//==========================================================================================
+
+		private static int staticInstanceCounter;
+
+		#endregion
+
+		#region Fields
+		//==========================================================================================
+		// Fields
+		//==========================================================================================
+
+		private int instanceId;
+		private bool isDisposed;
+
+		private System.Net.IPAddress localIPAddress;
+		private int localPort;
+
+		private SocketState state = SocketState.Reset;
+		private object stateSyncObj = new object();
+
+		private ALAZ.SystemEx.NetEx.SocketsEx.SocketServer socket;
+		private List<ALAZ.SystemEx.NetEx.SocketsEx.ISocketConnection> socketConnections = new List<ALAZ.SystemEx.NetEx.SocketsEx.ISocketConnection>();
+
+		private Queue<byte> receiveQueue = new Queue<byte>();
+
+		#endregion
+
+		#region Events
+		//==========================================================================================
+		// Events
+		//==========================================================================================
+
+		/// <summary></summary>
+		public event EventHandler IOChanged;
+
+		/// <summary></summary>
+		public event EventHandler IOControlChanged;
+
+		/// <summary></summary>
+		public event EventHandler DataReceived;
+
+		/// <summary></summary>
+		public event EventHandler DataSent;
+
+		/// <summary></summary>
+		public event EventHandler<IORequestEventArgs> IORequest;
+
+		/// <summary></summary>
+		public event EventHandler<IOErrorEventArgs> IOError;
+
+		#endregion
+
+		#region Object Lifetime
+		//==========================================================================================
+		// Object Lifetime
+		//==========================================================================================
+
+		/// <summary></summary>
+		public TcpServer(System.Net.IPAddress localIPAddress, int localPort)
+		{
+			this.instanceId = staticInstanceCounter++;
+
+			this.localIPAddress = localIPAddress;
+			this.localPort = localPort;
+		}
+
+		#region Disposal
+		//------------------------------------------------------------------------------------------
+		// Disposal
+		//------------------------------------------------------------------------------------------
+
+		/// <summary></summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary></summary>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.isDisposed)
+			{
+				if (disposing)
+				{
+					DisposeSocketAndSocketConnections();
+				}
+				this.isDisposed = true;
+
+				Debug.WriteLine(GetType() + "     (" + this.instanceId + ")(" + ToShortEndPointString() + "                  ): Disposed.");
+			}
+		}
+
+		/// <summary></summary>
+		~TcpServer()
+		{
+			Dispose(false);
+		}
+
+		/// <summary></summary>
+		protected bool IsDisposed
+		{
+			get { return (this.isDisposed); }
+		}
+
+		/// <summary></summary>
+		protected void AssertNotDisposed()
+		{
+			if (this.isDisposed)
+				throw (new ObjectDisposedException(GetType().ToString(), "Object has already been disposed"));
+		}
+
+		#endregion
+
+		#endregion
+
+		#region Properties
+		//==========================================================================================
+		// Properties
+		//==========================================================================================
+
+		/// <summary></summary>
+		public virtual System.Net.IPAddress LocalIPAddress
+		{
+			get
+			{
+				AssertNotDisposed();
+				return (this.localIPAddress);
+			}
+		}
+
+		/// <summary></summary>
+		public virtual int LocalPort
+		{
+			get
+			{
+				AssertNotDisposed();
+				return (this.localPort);
+			}
+		}
+
+		/// <summary></summary>
+		public virtual bool IsStopped
+		{
+			get
+			{
+				AssertNotDisposed();
+				switch (this.state)
+				{
+					case SocketState.Reset:
+					case SocketState.Stopping:
+					case SocketState.Error:
+					{
+						return (true);
+					}
+					default:
+					{
+						return (false);
+					}
+				}
+			}
+		}
+
+		/// <summary></summary>
+		public virtual bool IsStarted
+		{
+			get { return (!IsStopped); }
+		}
+
+		/// <summary></summary>
+		public virtual bool IsConnected
+		{
+			get
+			{
+				AssertNotDisposed();
+				switch (this.state)
+				{
+					case SocketState.Accepted:
+					{
+						return (true);
+					}
+					default:
+					{
+						return (false);
+					}
+				}
+			}
+		}
+
+		/// <summary></summary>
+		public virtual int ConnectedClientCount
+		{
+			get
+			{
+				AssertNotDisposed();
+				return (this.socketConnections.Count);
+			}
+		}
+
+		/// <summary></summary>
+		public virtual bool IsOpen
+		{
+			get { return (IsConnected); }
+		}
+
+		/// <summary></summary>
+		public virtual bool IsReadyToSend
+		{
+			get { return (IsConnected); }
+		}
+
+		/// <summary></summary>
+		public virtual int BytesAvailable
+		{
+			get
+			{
+				AssertNotDisposed();
+				return (this.receiveQueue.Count);
+			}
+		}
+
+		/// <summary></summary>
+		public virtual object UnderlyingIOInstance
+		{
+			get
+			{
+				AssertNotDisposed();
+				return (this.socket);
+			}
+		}
+
+		#endregion
+
+		#region Public Methods
+		//==========================================================================================
+		// Public Methods
+		//==========================================================================================
+
+		/// <summary></summary>
+		public virtual bool Start()
+		{
+			AssertNotDisposed();
+
+			if (!IsStarted)
+			{
+				StartSocket();
+				return (true);
+			}
+			else
+			{
+				Debug.WriteLine(GetType() + "     (" + this.instanceId + ")(" + ToShortEndPointString() + "                  ): Start() requested but state is " + this.state + ".");
+				return (false);
+			}
+		}
+
+		/// <summary></summary>
+		[SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Stop", Justification = "Stop is a common term to start/stop something.")]
+		public virtual void Stop()
+		{
+			AssertNotDisposed();
+
+			if (IsStarted)
+				StopSocket();
+			else
+				Debug.WriteLine(GetType() + "     (" + this.instanceId + ")(" + ToShortEndPointString() + "                  ): Stop() requested but state is " + this.state + ".");
+		}
+
+		/// <summary></summary>
+		public virtual int Receive(out byte[] data)
+		{
+			AssertNotDisposed();
+		
+			if (this.receiveQueue.Count > 0)
+			{
+				lock (this.receiveQueue)
+				{
+					int count = this.receiveQueue.Count;
+					data = new byte[count];
+					for (int i = 0; i < count; i++)
+						data[i] = this.receiveQueue.Dequeue();
+				}
+			}
+			else
+			{
+				data = new byte[] { };
+			}
+			return (data.Length);
+		}
+
+		/// <summary></summary>
+		public virtual void Send(byte[] data)
+		{
+			AssertNotDisposed();
+
+			if (IsStarted)
+			{
+				foreach (ALAZ.SystemEx.NetEx.SocketsEx.ISocketConnection connection in this.socketConnections)
+					connection.BeginSend(data);
+			}
+		}
+
+		#endregion
+
+		#region State Methods
+		//==========================================================================================
+		// State Methods
+		//==========================================================================================
+
+		private SocketState GetStateSynchronized()
+		{
+			SocketState state;
+
+			lock (this.stateSyncObj)
+				state = this.state;
+
+			return (state);
+		}
+
+		private void SetStateSynchronizedAndNotify(SocketState state)
+		{
+#if (DEBUG)
+			SocketState oldState = this.state;
+#endif
+			lock (this.stateSyncObj)
+				this.state = state;
+#if (DEBUG)
+			Debug.WriteLine(GetType() + "     (" + this.instanceId + ")(" + ToShortEndPointString() + "                  ): State has changed from " + oldState + " to " + this.state + ".");
+#endif
+			OnIOChanged(new EventArgs());
+		}
+
+		#endregion
+
+		#region Socket Methods
+		//==========================================================================================
+		// Socket Methods
+		//==========================================================================================
+
+		private void StartSocket()
+		{
+			SetStateSynchronizedAndNotify(SocketState.Listening);
+
+			this.socket = new ALAZ.SystemEx.NetEx.SocketsEx.SocketServer
+				(
+				ALAZ.SystemEx.NetEx.SocketsEx.CallbackThreadType.ctWorkerThread,
+				(ALAZ.SystemEx.NetEx.SocketsEx.ISocketService)this,
+				ALAZ.SystemEx.NetEx.SocketsEx.DelimiterType.dtNone,
+				null,
+				SocketDefaults.SocketBufferSize,
+				SocketDefaults.MessageBufferSize,
+				Timeout.Infinite,
+				Timeout.Infinite
+				);
+
+			this.socket.AddListener("YAT TCP Server Listener", new System.Net.IPEndPoint(System.Net.IPAddress.Any, this.localPort));
+			this.socket.Start(); // The ALAZ socket will be started asynchronously.
+		}
+
+		private void StopSocket()
+		{
+			SetStateSynchronizedAndNotify(SocketState.Stopping);
+
+			// \remind:
+			// The ALAZ sockets by default stop synchronously. However, due to some other issues
+			//   the ALAZ sockets had to be modified. The modified version stops asynchronously.
+			this.socket.Stop();
+
+			// \remind:
+			// See above
+			//SetStateAndNotify(SocketState.Reset);
+		}
+
+		private void DisposeSocketAndSocketConnections()
+		{
+			if (this.socket != null)
+			{
+				this.socket.Stop();
+				this.socket.Dispose(); // Attention: ALAZ sockets don't properly stop on Dispose().
+				this.socket = null;
+				this.socketConnections.Clear();
+			}
+		}
+
+		#endregion
+
+		#region ISocketService Members
+		//==========================================================================================
+		// ISocketService Members
+		//==========================================================================================
+
+		/// <summary>
+		/// Fired when connected.
+		/// </summary>
+		/// <param name="e">
+		/// Information about the connection.
+		/// </param>
+		public virtual void OnConnected(ALAZ.SystemEx.NetEx.SocketsEx.ConnectionEventArgs e)
+		{
+			lock (this.socketConnections)
+				this.socketConnections.Add(e.Connection);
+
+			SetStateSynchronizedAndNotify(SocketState.Accepted);
+
+			// Immediately begin receiving data.
+			e.Connection.BeginReceive();
+		}
+
+		/// <summary>
+		/// Fired when data arrives.
+		/// </summary>
+		/// <param name="e">
+		/// Information about the Message.
+		/// </param>
+		public virtual void OnReceived(ALAZ.SystemEx.NetEx.SocketsEx.MessageEventArgs e)
+		{
+			lock (this.receiveQueue)
+			{
+				foreach (byte b in e.Buffer)
+					this.receiveQueue.Enqueue(b);
+			}
+			OnDataReceived(new EventArgs());
+
+			// Continue receiving data.
+			e.Connection.BeginReceive();
+		}
+
+		/// <summary>
+		/// Fired when data is sent.
+		/// </summary>
+		/// <param name="e">
+		/// Information about the Message.
+		/// </param>
+		public virtual void OnSent(ALAZ.SystemEx.NetEx.SocketsEx.MessageEventArgs e)
+		{
+			OnDataSent(new EventArgs());
+		}
+
+		/// <summary>
+		/// Fired when disconnected.
+		/// </summary>
+		/// <param name="e">
+		/// Information about the connection.
+		/// </param>
+		public virtual void OnDisconnected(ALAZ.SystemEx.NetEx.SocketsEx.ConnectionEventArgs e)
+		{
+			bool isConnected = false;
+			lock (this.socketConnections)
+			{
+				this.socketConnections.Remove(e.Connection);
+				isConnected = (this.socketConnections.Count > 0);
+			}
+
+			if (!isConnected)
+			{
+				SocketState state = GetStateSynchronized();
+				switch (state)
+				{
+					case SocketState.Accepted: SetStateSynchronizedAndNotify(SocketState.Listening); break;
+					case SocketState.Stopping: SetStateSynchronizedAndNotify(SocketState.Reset);     break;
+					default: break; // No state change in all other cases.
+				}
+			}
+		}
+
+		/// <summary>
+		/// Fired when exception occurs.
+		/// </summary>
+		/// <param name="e">
+		/// Information about the exception and connection.
+		/// </param>
+		public virtual void OnException(ALAZ.SystemEx.NetEx.SocketsEx.ExceptionEventArgs e)
+		{
+			DisposeSocketAndSocketConnections();
+
+			SetStateSynchronizedAndNotify(SocketState.Error);
+			OnIOError(new IOErrorEventArgs(e.Exception.Message));
+		}
+
+		#endregion
+
+		#region Event Invoking
+		//==========================================================================================
+		// Event Invoking
+		//==========================================================================================
+
+		/// <summary></summary>
+		protected virtual void OnIOChanged(EventArgs e)
+		{
+			EventHelper.FireSync(IOChanged, this, e);
+		}
+
+		/// <summary></summary>
+		protected virtual void OnIOControlChanged(EventArgs e)
+		{
+			EventHelper.FireSync(IOControlChanged, this, e);
+			throw (new NotSupportedException("Event not in use"));
+		}
+
+		/// <summary></summary>
+		protected virtual void OnDataReceived(EventArgs e)
+		{
+			EventHelper.FireSync(DataReceived, this, e);
+		}
+
+		/// <summary></summary>
+		protected virtual void OnDataSent(EventArgs e)
+		{
+			EventHelper.FireSync(DataSent, this, e);
+		}
+
+		/// <summary></summary>
+		protected virtual void OnIORequest(IORequestEventArgs e)
+		{
+			EventHelper.FireSync(IORequest, this, e);
+			throw (new NotSupportedException("Event not in use"));
+		}
+
+		/// <summary></summary>
+		protected virtual void OnIOError(IOErrorEventArgs e)
+		{
+			EventHelper.FireSync<IOErrorEventArgs>(IOError, this, e);
+		}
+
+		#endregion
+
+		#region Object Members
+		//==========================================================================================
+		// Object Members
+		//==========================================================================================
+
+		/// <summary></summary>
+		/// <remarks>
+		/// Named according to .NET <see cref="System.Net.IPEndPoint"/>.
+		/// </remarks>
+		[SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly", MessageId = "EndPoint", Justification = "Naming according to System.Net.EndPoint.")]
+		public virtual string ToShortEndPointString()
+		{
+			return ("Server:" + this.localPort);
+		}
+
+		#endregion
+	}
+}
+
+//==================================================================================================
+// End of
+// $URL: https://y-a-terminal.svn.sourceforge.net/svnroot/y-a-terminal/trunk/MKY/MKY.IO.Serial/Socket/TcpServer.cs $
+//==================================================================================================
