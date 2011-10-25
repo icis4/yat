25fee22250f9440cd7f310273ab7851ca950cff1
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2011-10-25 19:21:16 +0000

    "Unhandled Exception" now shows all information including inner exceptions (bug #3290314)

--- trunk/YAT/YAT.Gui.Forms/UnhandledException.Designer.cs
+++ trunk/YAT/YAT.Gui.Forms/UnhandledException.Designer.cs
@@ -34,14 +34,7 @@ namespace YAT.Gui.Forms
 		{
 			this.button_Close = new System.Windows.Forms.Button();
 			this.groupBox_Exception = new System.Windows.Forms.GroupBox();
-			this.textBox_Source = new System.Windows.Forms.TextBox();
-			this.textBox_Message = new System.Windows.Forms.TextBox();
-			this.label_Message = new System.Windows.Forms.Label();
-			this.label_Type = new System.Windows.Forms.Label();
-			this.label_Source = new System.Windows.Forms.Label();
-			this.textBox_Type = new System.Windows.Forms.TextBox();
-			this.label_Stack = new System.Windows.Forms.Label();
-			this.textBox_Stack = new System.Windows.Forms.TextBox();
+			this.textBox_Exception = new System.Windows.Forms.TextBox();
 			this.button_CopyToClipboard = new System.Windows.Forms.Button();
 			this.button_Instructions = new System.Windows.Forms.Button();
 			this.groupBox_Message = new System.Windows.Forms.GroupBox();
@@ -64,14 +57,7 @@ namespace YAT.Gui.Forms
 			// 
 			this.groupBox_Exception.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
 						| System.Windows.Forms.AnchorStyles.Right)));
-			this.groupBox_Exception.Controls.Add(this.textBox_Source);
-			this.groupBox_Exception.Controls.Add(this.textBox_Message);
-			this.groupBox_Exception.Controls.Add(this.label_Message);
-			this.groupBox_Exception.Controls.Add(this.label_Type);
-			this.groupBox_Exception.Controls.Add(this.label_Source);
-			this.groupBox_Exception.Controls.Add(this.textBox_Type);
-			this.groupBox_Exception.Controls.Add(this.label_Stack);
-			this.groupBox_Exception.Controls.Add(this.textBox_Stack);
+			this.groupBox_Exception.Controls.Add(this.textBox_Exception);
 			this.groupBox_Exception.Location = new System.Drawing.Point(12, 84);
 			this.groupBox_Exception.Name = "groupBox_Exception";
 			this.groupBox_Exception.Size = new System.Drawing.Size(570, 346);
@@ -79,99 +65,21 @@ namespace YAT.Gui.Forms
 			this.groupBox_Exception.TabStop = false;
 			this.groupBox_Exception.Text = "Exception";
 			// 
-			// textBox_Source
+			// textBox_Exception
 			// 
-			this.textBox_Source.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+			this.textBox_Exception.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
 						| System.Windows.Forms.AnchorStyles.Right)));
-			this.textBox_Source.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.textBox_Source.Location = new System.Drawing.Point(68, 144);
-			this.textBox_Source.Name = "textBox_Source";
-			this.textBox_Source.ReadOnly = true;
-			this.textBox_Source.Size = new System.Drawing.Size(496, 13);
-			this.textBox_Source.TabIndex = 5;
-			this.textBox_Source.Text = "<SOURCE>";
-			// 
-			// textBox_Message
-			// 
-			this.textBox_Message.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.textBox_Message.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.textBox_Message.Location = new System.Drawing.Point(68, 48);
-			this.textBox_Message.Multiline = true;
-			this.textBox_Message.Name = "textBox_Message";
-			this.textBox_Message.ReadOnly = true;
-			this.textBox_Message.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.textBox_Message.Size = new System.Drawing.Size(496, 88);
-			this.textBox_Message.TabIndex = 3;
-			this.textBox_Message.Text = "<MESSAGE>";
-			this.textBox_Message.WordWrap = false;
-			// 
-			// label_Message
-			// 
-			this.label_Message.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.label_Message.AutoSize = true;
-			this.label_Message.Location = new System.Drawing.Point(12, 48);
-			this.label_Message.Name = "label_Message";
-			this.label_Message.Size = new System.Drawing.Size(53, 13);
-			this.label_Message.TabIndex = 2;
-			this.label_Message.Text = "Message:";
-			// 
-			// label_Type
-			// 
-			this.label_Type.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.label_Type.AutoSize = true;
-			this.label_Type.Location = new System.Drawing.Point(12, 24);
-			this.label_Type.Name = "label_Type";
-			this.label_Type.Size = new System.Drawing.Size(34, 13);
-			this.label_Type.TabIndex = 0;
-			this.label_Type.Text = "Type:";
-			// 
-			// label_Source
-			// 
-			this.label_Source.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.label_Source.AutoSize = true;
-			this.label_Source.Location = new System.Drawing.Point(12, 144);
-			this.label_Source.Name = "label_Source";
-			this.label_Source.Size = new System.Drawing.Size(44, 13);
-			this.label_Source.TabIndex = 4;
-			this.label_Source.Text = "Source:";
-			// 
-			// textBox_Type
-			// 
-			this.textBox_Type.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.textBox_Type.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.textBox_Type.Location = new System.Drawing.Point(68, 24);
-			this.textBox_Type.Name = "textBox_Type";
-			this.textBox_Type.ReadOnly = true;
-			this.textBox_Type.Size = new System.Drawing.Size(496, 13);
-			this.textBox_Type.TabIndex = 1;
-			this.textBox_Type.Text = "<TYPE>";
-			// 
-			// label_Stack
-			// 
-			this.label_Stack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.label_Stack.AutoSize = true;
-			this.label_Stack.Location = new System.Drawing.Point(12, 168);
-			this.label_Stack.Name = "label_Stack";
-			this.label_Stack.Size = new System.Drawing.Size(38, 13);
-			this.label_Stack.TabIndex = 6;
-			this.label_Stack.Text = "Stack:";
-			// 
-			// textBox_Stack
-			// 
-			this.textBox_Stack.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.textBox_Stack.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.textBox_Stack.Location = new System.Drawing.Point(68, 168);
-			this.textBox_Stack.Multiline = true;
-			this.textBox_Stack.Name = "textBox_Stack";
-			this.textBox_Stack.ReadOnly = true;
-			this.textBox_Stack.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.textBox_Stack.Size = new System.Drawing.Size(496, 172);
-			this.textBox_Stack.TabIndex = 7;
-			this.textBox_Stack.Text = "<STACK>";
-			this.textBox_Stack.WordWrap = false;
+			this.textBox_Exception.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox_Exception.Location = new System.Drawing.Point(9, 19);
+			this.textBox_Exception.Multiline = true;
+			this.textBox_Exception.Name = "textBox_Exception";
+			this.textBox_Exception.ReadOnly = true;
+			this.textBox_Exception.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox_Exception.Size = new System.Drawing.Size(555, 321);
+			this.textBox_Exception.TabIndex = 3;
+			this.textBox_Exception.Text = "<EXCEPTION>";
+			this.textBox_Exception.WordWrap = false;
 			// 
 			// button_CopyToClipboard
 			// 
@@ -204,7 +112,7 @@ namespace YAT.Gui.Forms
 			this.groupBox_Message.Controls.Add(this.label_Explanation);
 			this.groupBox_Message.Location = new System.Drawing.Point(12, 12);
 			this.groupBox_Message.Name = "groupBox_Message";
-			this.groupBox_Message.Size = new System.Drawing.Size(564, 68);
+			this.groupBox_Message.Size = new System.Drawing.Size(570, 68);
 			this.groupBox_Message.TabIndex = 0;
 			this.groupBox_Message.TabStop = false;
 			// 
@@ -216,7 +124,7 @@ namespace YAT.Gui.Forms
 			this.label_Explanation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label_Explanation.Location = new System.Drawing.Point(6, 16);
 			this.label_Explanation.Name = "label_Explanation";
-			this.label_Explanation.Size = new System.Drawing.Size(552, 49);
+			this.label_Explanation.Size = new System.Drawing.Size(558, 49);
 			this.label_Explanation.TabIndex = 1;
 			this.label_Explanation.Text = "An unhandled exception occured in YAT. Report this exception to YAT > Tracker > B" +
 				"ugs on SourceForge.net to give valuable feedback to continuously improve YAT. Se" +
@@ -251,16 +159,9 @@ namespace YAT.Gui.Forms
 
 		#endregion
 
-		private System.Windows.Forms.TextBox textBox_Source;
 		private System.Windows.Forms.Button button_Close;
 		private System.Windows.Forms.GroupBox groupBox_Exception;
-		private System.Windows.Forms.Label label_Message;
-		private System.Windows.Forms.Label label_Type;
-		private System.Windows.Forms.Label label_Source;
-		private System.Windows.Forms.TextBox textBox_Type;
-		private System.Windows.Forms.Label label_Stack;
-		private System.Windows.Forms.TextBox textBox_Stack;
-		private System.Windows.Forms.TextBox textBox_Message;
+		private System.Windows.Forms.TextBox textBox_Exception;
 		private System.Windows.Forms.Button button_CopyToClipboard;
 		private System.Windows.Forms.Button button_Instructions;
 		private System.Windows.Forms.GroupBox groupBox_Message;
