85170310ecbc24e339e2f33da14d18c1e75d3fe3
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2013-02-03 19:59:28 +0000

    1.99.30 post-release finalization, versions incremented to MKY 1.0.10 and YAT 1.99.31

--- MKY/MKY/Settings/EventArgs.cs
+++ MKY/MKY/Settings/EventArgs.cs
@@ -8,7 +8,7 @@
 // $Date$
 // $Revision$
 // ------------------------------------------------------------------------------------------------
-// MKY Version 1.0.9
+// MKY Development Version 1.0.10
 // ------------------------------------------------------------------------------------------------
 // See SVN change log for revision details.
 // See release notes for product version details.
