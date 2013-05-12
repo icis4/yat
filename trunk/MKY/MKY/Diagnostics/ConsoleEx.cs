e0861cf66c9864ecb7ec62357e6e611c4c408511
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2013-05-12 16:15:37 +0000

    Command line console output is now channeled to stdout or stderr depending on whether it is normal or error information (feature request #192)

--- trunk/MKY/MKY/Diagnostics/ConsoleEx.cs
+++ trunk/MKY/MKY/Diagnostics/ConsoleEx.cs
@@ -30,96 +30,129 @@ using System.Windows.Forms;
 namespace MKY.Diagnostics
 {
 	/// <summary>
-	/// Provides static methods to write diagnostics output to <see cref="System.Console"/>.
+	/// Provides static methods to write diagnostics output to <see cref="System.Console.Out"/> and
+	/// <see cref="System.Console.Error"/>.
 	/// </summary>
 	[SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix", Justification = "'Ex' emphasizes that it's an extension to an existing class and not a replacement as '2' would emphasize.")]
 	public static class ConsoleEx
 	{
-		private static ConsoleWrapper consoleWrapper = new ConsoleWrapper();
+		#region TextWriterEx
+		//==========================================================================================
+		// TextWriterEx
+		//==========================================================================================
 
 		/// <summary>
-		/// Writes source, type, message and stack of the given exception and its inner exceptions
-		/// to <see cref="System.Console"/>.
+		/// A console writer extension to <see cref="TextWriter"/>.
 		/// </summary>
-		public static void WriteException(Type type, Exception ex)
+		[SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix", Justification = "'Ex' emphasizes that it's an extension to an existing class and not a replacement as '2' would emphasize.")]
+		public class TextWriterEx
 		{
-			WriteException(type, ex, "");
+			private IDiagnosticsWriter writer;
+
+			/// <summary>
+			/// Initializes writer and sets the diagnostics writer that wraps the console writer.
+			/// </summary>
+			public TextWriterEx(IDiagnosticsWriter writer)
+			{
+				this.writer = writer;
+			}
+
+			/// <summary>
+			/// Writes source, type, message and stack of the given exception and its inner
+			/// exceptions to the output writer of this object.
+			/// </summary>
+			public void WriteException(Type type, Exception ex)
+			{
+				WriteException(type, ex, "");
+			}
+
+			/// <summary>
+			/// Writes source, type, message and stack of the given exception and its inner
+			/// exceptions to the output writer of this object.
+			/// </summary>
+			public void WriteException(Type type, Exception ex, string leadMessage)
+			{
+				DiagnosticsWriterOutput.WriteException(writer, type, ex, leadMessage);
+			}
+
+			/// <summary>
+			/// Writes a <see cref="StackTrace"/> to the output writer of this object.
+			/// </summary>
+			public void WriteStack(Type type)
+			{
+				WriteStack(type, new StackTrace(), "");
+			}
+
+			/// <summary>
+			/// Writes a <see cref="StackTrace"/> to the output writer of this object.
+			/// </summary>
+			public void WriteStack(Type type, string leadMessage)
+			{
+				WriteStack(type, new StackTrace(), leadMessage);
+			}
+
+			/// <summary>
+			/// Writes a <see cref="StackTrace"/> to the output writer of this object.
+			/// </summary>
+			public void WriteStack(Type type, StackTrace st)
+			{
+				WriteStack(type, st, "");
+			}
+
+			/// <summary>
+			/// Writes a <see cref="StackTrace"/> to the output writer of this object.
+			/// </summary>
+			public void WriteStack(Type type, StackTrace st, string leadMessage)
+			{
+				DiagnosticsWriterOutput.WriteStack(writer, type, st, leadMessage);
+			}
+
+			/// <summary>
+			/// Writes the properties of a <see cref="Message"/> to the output writer of this object.
+			/// </summary>
+			[SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "m", Justification = "Naming according to parameter 'm' of NativeWindow methods.")]
+			public void WriteWindowsFormsMessage(Type type, Message m)
+			{
+				WriteWindowsFormsMessage(type, m, "");
+			}
+
+			/// <summary>
+			/// Writes the properties of a <see cref="Message"/> to the output writer of this object.
+			/// </summary>
+			[SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "m", Justification = "Naming according to parameter 'm' of NativeWindow methods.")]
+			public void WriteWindowsFormsMessage(Type type, Message m, string leadMessage)
+			{
+				DiagnosticsWriterOutput.WriteWindowsFormsMessage(writer, type, m, leadMessage);
+			}
+
+			/// <summary>
+			/// Writes the properties of a <see cref="FileStream"/> to the output writer of this object.
+			/// </summary>
+			public void WriteFileStream(Type type, FileStream fs)
+			{
+				WriteFileStream(type, fs, "");
+			}
+
+			/// <summary>
+			/// Writes the properties of a <see cref="FileStream"/> to the output writer of this object.
+			/// </summary>
+			public void WriteFileStream(Type type, FileStream fs, string leadMessage)
+			{
+				DiagnosticsWriterOutput.WriteFileStream(writer, type, fs, leadMessage);
+			}
 		}
 
-		/// <summary>
-		/// Writes source, type, message and stack of the given exception and its inner exceptions
-		/// to <see cref="System.Console"/>.
-		/// </summary>
-		public static void WriteException(Type type, Exception ex, string leadMessage)
-		{
-			DiagnosticsWriterOutput.WriteException(consoleWrapper, type, ex, leadMessage);
-		}
+		#endregion
 
 		/// <summary>
-		/// Writes a <see cref="StackTrace"/> to <see cref="System.Console"/>.
+		/// Standard output.
 		/// </summary>
-		public static void WriteStack(Type type)
-		{
-			WriteStack(type, new StackTrace(), "");
-		}
+		public static TextWriterEx Out = new TextWriterEx(new ConsoleWrapper(Console.Out));
 
 		/// <summary>
-		/// Writes a <see cref="StackTrace"/> to <see cref="System.Console"/>.
+		/// Error output.
 		/// </summary>
-		public static void WriteStack(Type type, string leadMessage)
-		{
-			WriteStack(type, new StackTrace(), leadMessage);
-		}
-
-		/// <summary>
-		/// Writes a <see cref="StackTrace"/> to <see cref="System.Console"/>.
-		/// </summary>
-		public static void WriteStack(Type type, StackTrace st)
-		{
-			WriteStack(type, st, "");
-		}
-
-		/// <summary>
-		/// Writes a <see cref="StackTrace"/> to <see cref="System.Console"/>.
-		/// </summary>
-		public static void WriteStack(Type type, StackTrace st, string leadMessage)
-		{
-			DiagnosticsWriterOutput.WriteStack(consoleWrapper, type, st, leadMessage);
-		}
-
-		/// <summary>
-		/// Writes the properties of a <see cref="Message"/> to <see cref="System.Console"/>.
-		/// </summary>
-		[SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "m", Justification = "Naming according to parameter 'm' of NativeWindow methods.")]
-		public static void WriteWindowsFormsMessage(Type type, Message m)
-		{
-			WriteWindowsFormsMessage(type, m, "");
-		}
-
-		/// <summary>
-		/// Writes the properties of a <see cref="Message"/> to <see cref="System.Console"/>.
-		/// </summary>
-		[SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "m", Justification = "Naming according to parameter 'm' of NativeWindow methods.")]
-		public static void WriteWindowsFormsMessage(Type type, Message m, string leadMessage)
-		{
-			DiagnosticsWriterOutput.WriteWindowsFormsMessage(consoleWrapper, type, m, leadMessage);
-		}
-
-		/// <summary>
-		/// Writes the properties of a <see cref="FileStream"/> to <see cref="System.Console"/>.
-		/// </summary>
-		public static void WriteFileStream(Type type, FileStream fs)
-		{
-			WriteFileStream(type, fs, "");
-		}
-
-		/// <summary>
-		/// Writes the properties of a <see cref="FileStream"/> to <see cref="System.Console"/>.
-		/// </summary>
-		public static void WriteFileStream(Type type, FileStream fs, string leadMessage)
-		{
-			DiagnosticsWriterOutput.WriteFileStream(consoleWrapper, type, fs, leadMessage);
-		}
+		public static TextWriterEx Error = new TextWriterEx(new ConsoleWrapper(Console.Error));
 	}
 }
 
