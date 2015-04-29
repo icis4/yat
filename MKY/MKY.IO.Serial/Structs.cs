3f76d8435af72cc5410ea79c196398a8d7ab0105
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2015-04-29 20:00:21 +0000

    Mismatches in copyright years fixed

--- MKY/MKY.IO.Serial/Structs.cs
+++ MKY/MKY.IO.Serial/Structs.cs
@@ -13,8 +13,7 @@
 // See SVN change log for revision details.
 // See release notes for product version details.
 // ------------------------------------------------------------------------------------------------
-// Copyright © 2003-2004 HSR Hochschule für Technik Rapperswil.
-// Copyright © 2003-2013 Matthias Kläy.
+// Copyright © 2007-2015 Matthias Kläy.
 // All rights reserved.
 // ------------------------------------------------------------------------------------------------
 // This source code is licensed under the GNU LGPL.
