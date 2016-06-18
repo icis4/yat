ea531989242e1274057569e3b95ff418c97a2733
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2016-06-18 17:29:22 +0000

    Preparation of YAT 1.99.50: netrtfwriter versioning and releasing prepared

--- netrtfwriter/RtfWriter/Properties/AssemblyInfo.cs
+++ netrtfwriter/RtfWriter/Properties/AssemblyInfo.cs
@@ -3,7 +3,6 @@
 // the location: "..\..\RtfDemo\RtfDemo\bin\"
 // ------------------------------------------------------------------------------
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -35,5 +34,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.1.3")]
-[assembly: AssemblyFileVersion("0.9.1.3")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.13.*")] // Originally "0.9.1.3"
+[assembly: AssemblyFileVersion("0.9.13.0")] // Originally "0.9.1.3"
+[assembly: AssemblyInformationalVersion("0.9.13")]
