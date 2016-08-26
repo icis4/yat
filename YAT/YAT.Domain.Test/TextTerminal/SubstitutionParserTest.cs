7b40ad3eb1239ae70d00503f984595c72a708665
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2016-08-26 05:44:50 +0000

    SubstitutionParser test fixed

--- YAT/YAT.Domain.Test/TextTerminal/SubstitutionParserTest.cs
+++ YAT/YAT.Domain.Test/TextTerminal/SubstitutionParserTest.cs
@@ -67,7 +67,7 @@ namespace YAT.Domain.Test.TextTerminal
 		[Test, TestCaseSource(typeof(SubstitutionParserTestData), "TestCases")]
 		public virtual void TestSubstitutionParser(CharSubstitution substitution, string s, byte[] expectedBytes)
 		{
-			using (Domain.Parser.SubstitutionParser p = new Domain.Parser.SubstitutionParser())
+			using (Domain.Parser.SubstitutionParser p = new Domain.Parser.SubstitutionParser(substitution))
 			{
 				byte[] actualBytes = p.Parse(s);
 				Assert.AreEqual(expectedBytes, actualBytes);
