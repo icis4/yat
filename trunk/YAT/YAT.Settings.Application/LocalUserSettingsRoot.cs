566ee95f549d83a76b6567744c245f8726cef36a
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2016-09-02 09:12:48 +0000

    Preparing YAT 1.99.50: Settings updated

--- trunk/YAT/YAT.Settings.Application/LocalUserSettingsRoot.cs
+++ trunk/YAT/YAT.Settings.Application/LocalUserSettingsRoot.cs
@@ -34,10 +34,10 @@ namespace YAT.Settings.Application
 	[XmlRoot("LocalUserSettings")]
 	public class LocalUserSettingsRoot : MKY.Settings.SettingsItem, MKY.Xml.IAlternateXmlElementProvider
 	{
-		/// <remarks>Is basically constant, but must be a normal variable for automatic XML serialization.</remarks>
-		private string settingsVersion = "1.4.1";
+		/// <remarks>Is basically constant, but must be a variable for automatic XML serialization.</remarks>
+		private string settingsVersion = "1.5.0";
 
-		/// <remarks>Is basically constant, but must be a normal variable for automatic XML serialization.</remarks>
+		/// <remarks>Is basically constant, but must be a variable for automatic XML serialization.</remarks>
 		private string productVersion = ApplicationEx.ProductVersion;
 
 		private GeneralSettings general;
