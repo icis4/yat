7b4afde33b10c53b68489b27d5c949d62506b291
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2016-09-30 16:59:43 +0000

    YAT 2.0 Gamma 2'' Version 1.99.52 fixed

--- trunk/MKY/MKY.Version.cs
+++ trunk/MKY/MKY.Version.cs
@@ -36,9 +36,9 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.16.*")]
-[assembly: AssemblyFileVersion("1.0.16.0")]
-[assembly: AssemblyInformationalVersion("1.0.16")]
+[assembly: AssemblyVersion("1.0.17.*")]
+[assembly: AssemblyFileVersion("1.0.17.0")]
+[assembly: AssemblyInformationalVersion("1.0.17")]
 
 //==================================================================================================
 // End of
