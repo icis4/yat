f4da3bd3389ae929f9396aaeae3cfdf67cbb4326
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2016-09-30 17:00:28 +0000

    YAT 2.0 Gamma 2'' Version 1.99.52 invalid

--- tags/yat-1-99-52-gamma-2''/ALAZ/Demos/CertificateCreation/createCA.bat
+++
@@ -1,5 +0,0 @@
-openssl genrsa -out ca.key 2048
-
-openssl req -new -x509 -days 365 -sha256 -subj /C=BR/ST=TESTCA/L=TESTCA/O=TESTCA/OU=TESTCA/emailAddress=TESTCA -key ca.key -out ca.crt
-
-pause
--- tags/yat-1-99-52-gamma-2''/ALAZ/Demos/CertificateCreation/createPFX.bat
+++
@@ -1,3 +0,0 @@
-openssl pkcs12 -export -out cert.p12 -inkey cert.key -in cert.crt -chain -CAfile ca.crt
-
-pause
