d0e6fe160ee4e0dbc5be66129273eeffd690abcd
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2017-03-27 08:24:39 +0000

    Comments on MS version scheme added

--- MKY/MKY.Version.cs
+++ MKY/MKY.Version.cs
@@ -33,7 +33,8 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyVersion("1.0.*")] => 'Build' will indicate number of days since 2000-01-01.
+//                                      => 'Revision' will indicate number of seconds since midnight divided by two.
 [assembly: AssemblyVersion("1.0.18.*")]
 [assembly: AssemblyFileVersion("1.0.18.0")]
 [assembly: AssemblyInformationalVersion("1.0.18")]
