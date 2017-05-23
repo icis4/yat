e7a1d36df18b337b7c25a3c2d55dd08e4d1645ae
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2017-05-23 10:12:53 +0000

    Additional template added

---
+++ trunk/!-Doc.Developer/Template.cmd
@@ -0,0 +1,44 @@
+﻿@ECHO OFF
+
+::==================================================================================================
+:: YAT - Yet Another Terminal.
+:: Visit YAT at https://sourceforge.net/projects/y-a-terminal/.
+:: Contact YAT by mailto:y-a-terminal@users.sourceforge.net.
+:: ------------------------------------------------------------------------------------------------
+:: $URL: https://svn.code.sf.net/p/y-a-terminal/code/trunk/!-Scripts/CleanDir.cmd $
+:: $Revision: 1223 $
+:: $Date: 2017-01-15 17:07:25 +0100 (So, 15 Jan 2017) $
+:: $Author: maettu_this $
+:: ------------------------------------------------------------------------------------------------
+:: See release notes for product version details.
+:: See SVN change log for file revision details.
+:: Author(s): Matthias Klaey
+:: ------------------------------------------------------------------------------------------------
+:: Copyright © 2003-2017 Matthias Kläy.
+:: All rights reserved.
+:: ------------------------------------------------------------------------------------------------
+:: This source code is licensed under the GNU LGPL.
+:: See http://www.gnu.org/licenses/lgpl.html for license details.
+::==================================================================================================
+
+IF %1=="" GOTO ErrorHandler
+
+::--------------------------------------------------------------------------------------------------
+PUSHD %1
+
+ECHO Do something . . .
+
+POPD
+GOTO End
+::--------------------------------------------------------------------------------------------------
+
+:ErrorHandler
+ECHO Parameter 1 must be . . .
+GOTO End
+
+:End
+
+::==================================================================================================
+:: End of
+:: $URL: https://svn.code.sf.net/p/y-a-terminal/code/trunk/!-Scripts/CleanDir.cmd $
+::==================================================================================================
