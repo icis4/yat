bf337b844d8d5c7cf4b2d713162a7706b22cbb15
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2017-09-16 05:00:39 +0000

    Minor debug message improvement

--- MKY/MKY.IO.Serial.Socket/UdpSocket.cs
+++ MKY/MKY.IO.Serial.Socket/UdpSocket.cs
@@ -839,7 +839,7 @@ namespace MKY.IO.Serial.Socket
 			}
 			catch (ThreadAbortException ex)
 			{
-				DebugEx.WriteException(GetType(), ex, "SendThread() has been aborted! Confirming the abort, i.e. Thread.ResetAbort() will be called.");
+				DebugEx.WriteException(GetType(), ex, "SendThread() has been aborted! Confirming the abort, i.e. Thread.ResetAbort() will be called...");
 
 				// Should only happen when failing to 'friendly' join the thread on stopping!
 				// Don't try to set and notify a state change, or even restart the socket!
@@ -1365,7 +1365,7 @@ namespace MKY.IO.Serial.Socket
 			}
 			catch (ThreadAbortException ex)
 			{
-				DebugEx.WriteException(GetType(), ex, "ReceiveThread() has been aborted! Confirming the abort, i.e. Thread.ResetAbort() will be called.");
+				DebugEx.WriteException(GetType(), ex, "ReceiveThread() has been aborted! Confirming the abort, i.e. Thread.ResetAbort() will be called...");
 
 				// Should only happen when failing to 'friendly' join the thread on stopping!
 				// Don't try to set and notify a state change, or even restart the socket!
