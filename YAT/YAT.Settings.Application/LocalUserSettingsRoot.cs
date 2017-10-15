33d1bf8a1093799480e7a3025e31af3eb25b78f4
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2017-10-15 11:00:04 +0000

    Preparing 1.99.80: Default settings archived

--- YAT/YAT.Settings.Application/LocalUserSettingsRoot.cs
+++ YAT/YAT.Settings.Application/LocalUserSettingsRoot.cs
@@ -46,7 +46,7 @@ namespace YAT.Settings.Application
 	public class LocalUserSettingsRoot : MKY.Settings.SettingsItem, IEquatable<LocalUserSettingsRoot>, MKY.Xml.IAlternateXmlElementProvider
 	{
 		/// <remarks>Is basically constant, but must be a variable for automatic XML serialization.</remarks>
-		private string settingsVersion = "1.5.1";
+		private string settingsVersion = "1.5.2";
 
 		/// <remarks>Is basically constant, but must be a variable for automatic XML serialization.</remarks>
 		private string productVersion = ApplicationEx.ProductVersion;
