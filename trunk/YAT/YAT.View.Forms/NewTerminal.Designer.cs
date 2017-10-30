c01c78d92b598967dd5c25a762c9b8dca84cb719
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2017-10-30 18:12:30 +0000

    Open/Start and Close/Stop

--- trunk/YAT/YAT.View.Forms/NewTerminal.Designer.cs
+++ trunk/YAT/YAT.View.Forms/NewTerminal.Designer.cs
@@ -208,7 +208,7 @@ namespace YAT.View.Forms
 			this.checkBox_StartTerminal.Name = "checkBox_StartTerminal";
 			this.checkBox_StartTerminal.Size = new System.Drawing.Size(91, 17);
 			this.checkBox_StartTerminal.TabIndex = 2;
-			this.checkBox_StartTerminal.Text = "&Open terminal";
+			this.checkBox_StartTerminal.Text = "&Open/Start";
 			this.checkBox_StartTerminal.CheckedChanged += new System.EventHandler(this.checkBox_StartTerminal_CheckedChanged);
 			// 
 			// pictureBox_New
