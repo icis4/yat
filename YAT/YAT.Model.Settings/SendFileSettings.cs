514fcc1515e0d6181d06718f13ceaa7c46c778f2
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2018-04-06 15:11:02 +0000

    Comment improved related to command decoupling

--- YAT/YAT.Model.Settings/SendFileSettings.cs
+++ YAT/YAT.Model.Settings/SendFileSettings.cs
@@ -64,7 +64,7 @@ namespace YAT.Model.Settings
 		public SendFileSettings(SendFileSettings rhs)
 			: base(rhs)
 		{
-			Command = new Command(rhs.Command);
+			Command = new Command(rhs.Command); // Clone command to ensure decoupling.
 			RecentCommands = new RecentItemCollection<Command>(rhs.RecentCommands);
 			ClearChanged();
 		}
