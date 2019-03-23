03877f004af4645bc2b4a92f6ef02c5ef955cf0e
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2019-03-23 09:15:16 +0000

    Trailing whitespace removed

--- MKY/MKY.IO.Serial.SerialPort/Properties/AssemblyInfo.cs
+++ MKY/MKY.IO.Serial.SerialPort/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MKY.IO.Serial.SerialPort")]
@@ -38,12 +38,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components. If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
 [assembly: Guid("f8052941-8976-41bb-b1e6-0f3232ee4002")]
 
 // Assembly versions are defined by linked-in MKY.Version.cs.
--- MKY/MKY.IO.Serial.Socket.Test/Properties/AssemblyInfo.cs
+++ MKY/MKY.IO.Serial.Socket.Test/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MKY.IO.Serial.Socket.Test")]
@@ -38,12 +38,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components. If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
 [assembly: Guid("7f454929-e5e6-4300-807b-35f055e3969f")]
 
 // Assembly versions are defined by linked-in MKY.Version.cs.
