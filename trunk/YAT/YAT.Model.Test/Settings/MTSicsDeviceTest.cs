2fb4c063fe759d5fbf72cbbd0169d00a0b8fbe66
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2019-06-03 13:02:52 +0000

    Handling of test canceling improved

--- trunk/YAT/YAT.Model.Test/Settings/MTSicsDeviceTest.cs
+++ trunk/YAT/YAT.Model.Test/Settings/MTSicsDeviceTest.cs
@@ -111,7 +111,7 @@ namespace YAT.Model.Test.Settings
 			);
 
 			if (dr != DialogResult.OK)
-				Assert.Fail("User cancel!");
+				Assert.Ignore("Tester has canceled");
 
 			// Create temporary in-memory application settings for this test run:
 			ApplicationSettings.Create(ApplicationSettingsFileAccess.None);
