e25df859fcc80b7b901faa3180dfadb3e87d6844
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2019-07-22 23:52:25 +0000

    .yac / .yacp roots added

---
+++ YAT/YAT.Settings.Model/CommandPagesSettingsRoot.cs
@@ -0,0 +1,220 @@
+﻿//==================================================================================================
+// YAT - Yet Another Terminal.
+// Visit YAT at https://sourceforge.net/projects/y-a-terminal/.
+// Contact YAT by mailto:y-a-terminal@users.sourceforge.net.
+// ------------------------------------------------------------------------------------------------
+// $URL$
+// $Revision$
+// $Date$
+// $Author$
+// ------------------------------------------------------------------------------------------------
+// YAT Version 2.0.1 Development
+// ------------------------------------------------------------------------------------------------
+// See release notes for product version details.
+// See SVN change log for file revision details.
+// Author(s): Matthias Klaey
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2003-2004 HSR Hochschule für Technik Rapperswil.
+// Copyright © 2003-2019 Matthias Kläy.
+// All rights reserved.
+// ------------------------------------------------------------------------------------------------
+// This source code is licensed under the GNU LGPL.
+// See http://www.gnu.org/licenses/lgpl.html for license details.
+//==================================================================================================
+
+#region Using
+//==================================================================================================
+// Using
+//==================================================================================================
+
+using System;
+using System.Xml.Serialization;
+
+using MKY;
+
+using YAT.Application.Utilities;
+using YAT.Model.Settings;
+
+#endregion
+
+namespace YAT.Settings.Model
+{
+	/// <summary></summary>
+	[XmlRoot("Settings")]
+	public class CommandPagesSettingsRoot : MKY.Settings.SettingsItem, IEquatable<CommandPagesSettingsRoot>
+	{
+		/// <remarks>Is basically constant, but must be a variable for automatic XML serialization.</remarks>
+		private string settingsVersion = "1.0.0";
+
+		/// <remarks>Is basically constant, but must be a variable for automatic XML serialization.</remarks>
+		private string productVersion = ApplicationEx.ProductVersion;
+
+		private PredefinedCommandSettings predefinedCommand;
+
+		/// <summary></summary>
+		public CommandPagesSettingsRoot()
+			: base(MKY.Settings.SettingsType.Explicit)
+		{
+			PredefinedCommand = new PredefinedCommandSettings(MKY.Settings.SettingsType.Explicit);
+
+			ClearChanged();
+		}
+
+		/// <remarks>
+		/// Fields are assigned via properties even though changed flag will be cleared anyway.
+		/// There potentially is additional code that needs to be run within the property method.
+		/// </remarks>
+		public CommandPagesSettingsRoot(CommandPagesSettingsRoot rhs)
+			: base(rhs)
+		{
+			PredefinedCommand = new PredefinedCommandSettings(rhs.PredefinedCommand);
+
+			ClearChanged();
+		}
+
+		#region Properties
+		//==========================================================================================
+		// Properties
+		//==========================================================================================
+
+		/// <remarks>File type is a kind of title, therefore capital 'C' and 'D'.</remarks>
+		[XmlElement("FileType")]
+		public virtual string FileType
+		{
+			get { return (ApplicationEx.ProductName + " Command Definition"); } // File identification shall differ for "YAT" and "YATConsole".
+			set { } // Do nothing.
+		}
+
+		/// <summary></summary>
+		[XmlElement("SettingsVersion")]
+		public virtual string SettingsVersion
+		{
+			get { return (this.settingsVersion); }
+			set { } // Do nothing.
+		}
+
+		/// <summary></summary>
+		[XmlElement("ProductVersion")]
+		public virtual string ProductVersion
+		{
+			get { return (this.productVersion); }
+			set { } // Do nothing.
+		}
+
+		/// <summary></summary>
+		[XmlElement("Warning")]
+		public virtual string Warning
+		{
+			get { return ("Modifying structure and/or content may cause undefined behavior!"); }
+			set { } // Do nothing.
+		}
+
+		/// <summary></summary>
+		[XmlElement("Saved")]
+		public virtual SaveInfo Saved
+		{
+			get { return (new SaveInfo(DateTime.Now, Environment.UserName)); }
+			set { } // Do nothing.
+		}
+
+		/// <summary></summary>
+		[XmlElement("PredefinedCommand")]
+		public virtual PredefinedCommandSettings PredefinedCommand
+		{
+			get { return (this.predefinedCommand); }
+			set
+			{
+				if (this.predefinedCommand != value)
+				{
+					var oldNode = this.predefinedCommand;
+					this.predefinedCommand = value; // New node must be referenced before replacing node below! Replace will invoke the 'Changed' event!
+
+					AttachOrReplaceOrDetachNode(oldNode, value);
+				}
+			}
+		}
+
+		#endregion
+
+		#region Object Members
+		//==========================================================================================
+		// Object Members
+		//==========================================================================================
+
+		/// <summary>
+		/// Serves as a hash function for a particular type.
+		/// </summary>
+		/// <remarks>
+		/// Use properties instead of fields to calculate hash code. This ensures that 'intelligent'
+		/// properties, i.e. properties with some logic, are also properly handled.
+		/// </remarks>
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				int hashCode = base.GetHashCode(); // Get hash code of all settings nodes.
+
+				hashCode = (hashCode * 397) ^ (ProductVersion != null ? ProductVersion.GetHashCode() : 0);
+
+				return (hashCode);
+			}
+		}
+
+		/// <summary>
+		/// Determines whether this instance and the specified object have reference or value equality.
+		/// </summary>
+		public override bool Equals(object obj)
+		{
+			return (Equals(obj as CommandPagesSettingsRoot));
+		}
+
+		/// <summary>
+		/// Determines whether this instance and the specified object have reference or value equality.
+		/// </summary>
+		/// <remarks>
+		/// Use properties instead of fields to determine equality. This ensures that 'intelligent'
+		/// properties, i.e. properties with some logic, are also properly handled.
+		/// </remarks>
+		public bool Equals(CommandPagesSettingsRoot other)
+		{
+			if (ReferenceEquals(other, null)) return (false);
+			if (ReferenceEquals(this, other)) return (true);
+			if (GetType() != other.GetType()) return (false);
+
+			return
+			(
+				base.Equals(other) && // Compare all settings nodes.
+
+				StringEx.EqualsOrdinalIgnoreCase(ProductVersion, other.ProductVersion)
+			);
+		}
+
+		/// <summary>
+		/// Determines whether the two specified objects have reference or value equality.
+		/// </summary>
+		public static bool operator ==(CommandPagesSettingsRoot lhs, CommandPagesSettingsRoot rhs)
+		{
+			if (ReferenceEquals(lhs, rhs))  return (true);
+			if (ReferenceEquals(lhs, null)) return (false);
+			if (ReferenceEquals(rhs, null)) return (false);
+
+			object obj = (object)lhs; // Operators are not virtual! Calling object.Equals() ensures
+			return (obj.Equals(rhs)); // that a potential virtual <Derived>.Equals() is called.
+		}
+
+		/// <summary>
+		/// Determines whether the two specified objects have reference and value inequality.
+		/// </summary>
+		public static bool operator !=(CommandPagesSettingsRoot lhs, CommandPagesSettingsRoot rhs)
+		{
+			return (!(lhs == rhs));
+		}
+
+		#endregion
+	}
+}
+
+//==================================================================================================
+// End of
+// $URL$
+//==================================================================================================
