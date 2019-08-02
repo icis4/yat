126dbfdc35ea7c6ffde964d638e5f8c533af3375
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2019-08-02 07:39:35 +0000

    A > Y integration related reminder refined

--- YAT/YAT.Model.Settings/ViewSettings.cs
+++ YAT/YAT.Model.Settings/ViewSettings.cs
@@ -34,9 +34,11 @@ using MKY.Collections.Specialized;
 namespace YAT.Model.Settings
 {
 	/// <remarks>
-	/// \remind (2017-11-19 / MKY)
-	/// Could/should be migrated to 'YAT.Application.Settings' or 'YAT.View.Settings'.
-	/// To be done when refactoring the projects on integration with Albatros.
+	/// \remind (2017-11-19 / MKY), (2019-08-02 / MKY)
+	/// Should be migrated to a separate 'YAT.Application.Settings' project. Not easily possible
+	/// because of dependencies among 'YAT.*' and 'YAT.Application', e.g. 'ExtensionSettings'.
+	/// Requires slight refactoring of project dependecies. Could be done when refactoring the
+	/// projects on integration with Albatros.
 	/// </remarks>
 	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "'Albatros' is a name.")]
 	public class ViewSettings : MKY.Settings.SettingsItem, IEquatable<ViewSettings>
