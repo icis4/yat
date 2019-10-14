eb2b71240fa6998e762624ce66c90ddfdb767610
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2019-10-14 12:34:51 +0000

    Unused usings removed

--- trunk/YAT/YAT.Domain/BinaryTerminal/BinaryTerminal.cs
+++ trunk/YAT/YAT.Domain/BinaryTerminal/BinaryTerminal.cs
@@ -28,8 +28,6 @@
 //==================================================================================================
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading;
