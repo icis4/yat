6fe20e2be6ebf818c6d76f4cbba948881d2ee49a
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2019-10-25 08:48:21 +0000

    Another step to improve performance related to 'SystemFonts.DefaultFont' and 'DrawingEx.DefaultFontItalic'

--- trunk/YAT/YAT.View.Controls/PredefinedCommandButtonSet.Designer.cs
+++ trunk/YAT/YAT.View.Controls/PredefinedCommandButtonSet.Designer.cs
@@ -51,12 +51,14 @@
 			this.button_Command_8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_8.Enabled = false;
+			this.button_Command_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_8.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_8.Location = new System.Drawing.Point(0, 175);
 			this.button_Command_8.Name = "button_Command_8";
 			this.button_Command_8.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_8.TabIndex = 10;
 			this.button_Command_8.Tag = "8";
-			this.button_Command_8.Text = "<Undefined>";
+			this.button_Command_8.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_8, "Send command");
 			this.button_Command_8.Click += new System.EventHandler(this.button_Command_Click);
 			// 
@@ -65,12 +67,14 @@
 			this.button_Command_7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_7.Enabled = false;
+			this.button_Command_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_7.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_7.Location = new System.Drawing.Point(0, 153);
 			this.button_Command_7.Name = "button_Command_7";
 			this.button_Command_7.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_7.TabIndex = 9;
 			this.button_Command_7.Tag = "7";
-			this.button_Command_7.Text = "<Undefined>";
+			this.button_Command_7.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_7, "Send command");
 			this.button_Command_7.Click += new System.EventHandler(this.button_Command_Click);
 			// 
@@ -79,12 +83,14 @@
 			this.button_Command_6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_6.Enabled = false;
+			this.button_Command_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_6.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_6.Location = new System.Drawing.Point(0, 131);
 			this.button_Command_6.Name = "button_Command_6";
 			this.button_Command_6.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_6.TabIndex = 8;
 			this.button_Command_6.Tag = "6";
-			this.button_Command_6.Text = "<Undefined>";
+			this.button_Command_6.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_6, "Send command");
 			this.button_Command_6.Click += new System.EventHandler(this.button_Command_Click);
 			// 
@@ -93,12 +99,14 @@
 			this.button_Command_5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_5.Enabled = false;
+			this.button_Command_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_5.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_5.Location = new System.Drawing.Point(0, 109);
 			this.button_Command_5.Name = "button_Command_5";
 			this.button_Command_5.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_5.TabIndex = 7;
 			this.button_Command_5.Tag = "5";
-			this.button_Command_5.Text = "<Undefined>";
+			this.button_Command_5.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_5, "Send command");
 			this.button_Command_5.Click += new System.EventHandler(this.button_Command_Click);
 			// 
@@ -107,12 +115,14 @@
 			this.button_Command_4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_4.Enabled = false;
+			this.button_Command_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_4.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_4.Location = new System.Drawing.Point(0, 80);
 			this.button_Command_4.Name = "button_Command_4";
 			this.button_Command_4.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_4.TabIndex = 6;
 			this.button_Command_4.Tag = "4";
-			this.button_Command_4.Text = "<Undefined>";
+			this.button_Command_4.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_4, "Send command");
 			this.button_Command_4.Click += new System.EventHandler(this.button_Command_Click);
 			// 
@@ -121,12 +131,14 @@
 			this.button_Command_3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_3.Enabled = false;
+			this.button_Command_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_3.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_3.Location = new System.Drawing.Point(0, 58);
 			this.button_Command_3.Name = "button_Command_3";
 			this.button_Command_3.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_3.TabIndex = 5;
 			this.button_Command_3.Tag = "3";
-			this.button_Command_3.Text = "<Undefined>";
+			this.button_Command_3.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_3, "Send command");
 			this.button_Command_3.Click += new System.EventHandler(this.button_Command_Click);
 			// 
@@ -135,12 +147,14 @@
 			this.button_Command_2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_2.Enabled = false;
+			this.button_Command_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_2.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_2.Location = new System.Drawing.Point(0, 36);
 			this.button_Command_2.Name = "button_Command_2";
 			this.button_Command_2.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_2.TabIndex = 4;
 			this.button_Command_2.Tag = "2";
-			this.button_Command_2.Text = "<Undefined>";
+			this.button_Command_2.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_2, "Send command");
 			this.button_Command_2.Click += new System.EventHandler(this.button_Command_Click);
 			// 
@@ -149,12 +163,14 @@
 			this.button_Command_1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_1.Enabled = false;
+			this.button_Command_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_1.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_1.Location = new System.Drawing.Point(0, 14);
 			this.button_Command_1.Name = "button_Command_1";
 			this.button_Command_1.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_1.TabIndex = 3;
 			this.button_Command_1.Tag = "1";
-			this.button_Command_1.Text = "<Undefined>";
+			this.button_Command_1.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_1, "Send command");
 			this.button_Command_1.Click += new System.EventHandler(this.button_Command_Click);
 			// 
@@ -163,12 +179,14 @@
 			this.button_Command_9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_9.Enabled = false;
+			this.button_Command_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_9.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_9.Location = new System.Drawing.Point(0, 204);
 			this.button_Command_9.Name = "button_Command_9";
 			this.button_Command_9.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_9.TabIndex = 11;
 			this.button_Command_9.Tag = "9";
-			this.button_Command_9.Text = "<Undefined>";
+			this.button_Command_9.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_9, "Send command");
 			this.button_Command_9.Click += new System.EventHandler(this.button_Command_Click);
 			// 
@@ -177,13 +195,15 @@
 			this.button_Command_10.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_10.Enabled = false;
+			this.button_Command_10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_10.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_10.Location = new System.Drawing.Point(0, 226);
 			this.button_Command_10.Name = "button_Command_10";
 			this.button_Command_10.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_10.TabIndex = 12;
 			this.button_Command_10.Tag = "10";
-			this.button_Command_10.Text = "<Undefined>";
-			this.toolTip.SetToolTip(this.button_Command_10, "Send Command");
+			this.button_Command_10.Text = "<Define...>";
+			this.toolTip.SetToolTip(this.button_Command_10, "Send command");
 			this.button_Command_10.Click += new System.EventHandler(this.button_Command_Click);
 			// 
 			// button_Command_11
@@ -191,12 +211,14 @@
 			this.button_Command_11.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_11.Enabled = false;
+			this.button_Command_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_11.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_11.Location = new System.Drawing.Point(0, 248);
 			this.button_Command_11.Name = "button_Command_11";
 			this.button_Command_11.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_11.TabIndex = 13;
 			this.button_Command_11.Tag = "11";
-			this.button_Command_11.Text = "<Undefined>";
+			this.button_Command_11.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_11, "Send command");
 			this.button_Command_11.Click += new System.EventHandler(this.button_Command_Click);
 			// 
@@ -205,26 +227,16 @@
 			this.button_Command_12.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.button_Command_12.Enabled = false;
+			this.button_Command_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button_Command_12.ForeColor = System.Drawing.SystemColors.GrayText;
 			this.button_Command_12.Location = new System.Drawing.Point(0, 270);
 			this.button_Command_12.Name = "button_Command_12";
 			this.button_Command_12.Size = new System.Drawing.Size(176, 22);
 			this.button_Command_12.TabIndex = 14;
 			this.button_Command_12.Tag = "12";
-			this.button_Command_12.Text = "<Undefined>";
+			this.button_Command_12.Text = "<Define...>";
 			this.toolTip.SetToolTip(this.button_Command_12, "Send command");
 			this.button_Command_12.Click += new System.EventHandler(this.button_Command_Click);
-			// 
-			// label_Hint
-			// 
-			this.label_Hint.AutoSize = true;
-			this.label_Hint.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label_Hint.ForeColor = System.Drawing.SystemColors.GrayText;
-			this.label_Hint.Location = new System.Drawing.Point(0, 0);
-			this.label_Hint.Name = "label_Hint";
-			this.label_Hint.Size = new System.Drawing.Size(100, 13);
-			this.label_Hint.TabIndex = 1;
-			this.label_Hint.Text = "[Ctrl+] Shift+F1..F12 to send [to copy]";
-			this.label_Hint.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// label_SeparatorLine
 			// 
@@ -235,6 +247,18 @@
 			this.label_SeparatorLine.Name = "label_SeparatorLine";
 			this.label_SeparatorLine.Size = new System.Drawing.Size(172, 1);
 			this.label_SeparatorLine.TabIndex = 0;
+			// 
+			// label_Hint
+			// 
+			this.label_Hint.AutoSize = true;
+			this.label_Hint.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_Hint.ForeColor = System.Drawing.SystemColors.GrayText;
+			this.label_Hint.Location = new System.Drawing.Point(0, 0);
+			this.label_Hint.Name = "label_Hint";
+			this.label_Hint.Size = new System.Drawing.Size(182, 13);
+			this.label_Hint.TabIndex = 1;
+			this.label_Hint.Text = "[Ctrl+] Shift+F1..F12 to send [to copy]";
+			this.label_Hint.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// PredefinedCommandButtonSet
 			// 
