1a3e5de0ccb374ff0f9f5ed99fe979bee93e6a19
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2019-11-14 20:31:38 +0000

    Handling of test settings fixed

--- branches/yat-2-1-0-WithStepsTowards-2-1-1/YAT/YAT.Model.Test/Utilities.cs
+++ branches/yat-2-1-0-WithStepsTowards-2-1-1/YAT/YAT.Model.Test/Utilities.cs
@@ -540,6 +540,7 @@ namespace YAT.Model.Test
 			var settings = new TerminalSettingsRoot();
 			settings.TerminalType = Domain.TerminalType.Text;
 			settings.Terminal.TextTerminal.ShowEol = true; // Required for easier test verification (byte count).
+			settings.Terminal.UpdateTerminalTypeDependentSettings();
 			return (settings);
 		}
 
@@ -553,6 +554,8 @@ namespace YAT.Model.Test
 			var settings = GetTextSettings();
 			settings.Terminal.IO.IOType = Domain.IOType.SerialPort;
 			settings.Terminal.IO.SerialPort.PortId = portId;
+			settings.Terminal.UpdateIOTypeDependentSettings();
+			settings.Terminal.UpdateIOSettingsDependentSettings();
 			settings.TerminalIsStarted = true;
 			return (settings);
 		}
@@ -659,6 +662,8 @@ namespace YAT.Model.Test
 			var settings = GetTextSettings();
 			settings.Terminal.IO.IOType = type;
 			settings.Terminal.IO.Socket.LocalInterface = networkInterface;
+			settings.Terminal.UpdateIOTypeDependentSettings();
+			settings.Terminal.UpdateIOSettingsDependentSettings();
 			settings.TerminalIsStarted = true;
 			return (settings);
 		}
@@ -1082,6 +1087,8 @@ namespace YAT.Model.Test
 			var settings = GetTextSettings();
 			settings.Terminal.IO.IOType = Domain.IOType.UsbSerialHid;
 			settings.Terminal.IO.UsbSerialHidDevice.DeviceInfo = deviceInfo;
+			settings.Terminal.UpdateIOTypeDependentSettings();
+			settings.Terminal.UpdateIOSettingsDependentSettings();
 			settings.TerminalIsStarted = true;
 			return (settings);
 		}
@@ -1178,6 +1185,7 @@ namespace YAT.Model.Test
 			// MT-SICS devices use XOn/XOff by default:
 			settings.Terminal.IO.SerialPort.Communication.FlowControl = SerialFlowControl.Software;
 			settings.Terminal.IO.UsbSerialHidDevice      .FlowControl = SerialHidFlowControl.Software;
+			settings.Terminal.UpdateIOSettingsDependentSettings();
 
 			// Set required USB Ser/HID format incl. dependent settings:
 			var presetEx = new MKY.IO.Usb.SerialHidDeviceSettingsPresetEx(MKY.IO.Usb.SerialHidDeviceSettingsPreset.MT_SerHid);
