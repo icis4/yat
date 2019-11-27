cccef4c409f4972b7fd940976667d73b8df86138
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2019-11-27 13:40:12 +0000

    Empty line handling simplified; [YAT - Test.ods]::[YAT.Domain.Terminal] verified

--- trunk/YAT/YAT.Domain/TextTerminal/TextLineState.cs
+++ trunk/YAT/YAT.Domain/TextTerminal/TextLineState.cs
@@ -32,22 +32,20 @@ namespace YAT.Domain
 	public class TextLineState
 	{
 		/// <summary></summary>
-		public byte[]                            EolSequence                                { get; }
+		public byte[]                            EolSequence                          { get; }
 
 		/// <summary></summary>
-		public List<byte>                        PendingMultiBytesToDecode                  { get; private set; }
+		public List<byte>                        PendingMultiBytesToDecode            { get; private set; }
 
 		/// <summary></summary>
-		public int                               ShownCharCount                             { get; private set; }
+		public int                               ShownCharCount                       { get; private set; }
 
 		/// <summary></summary>
-		public Dictionary<string, SequenceQueue> EolOfGivenDevice                           { get; private set; }
+		public Dictionary<string, SequenceQueue> EolOfGivenDevice                     { get; private set; }
+
 
 		/// <summary></summary>
-		public Dictionary<string, bool>          EolOfLastLineOfGivenDeviceWasCompleteMatch { get; private set; }
-
-		/// <summary></summary>
-		public DisplayElementCollection          RetainedUnconfirmedHiddenEolElements       { get; private set; }
+		public DisplayElementCollection          RetainedUnconfirmedHiddenEolElements { get; private set; }
 
 		/// <summary></summary>
 		public TextLineState(byte[] eolSequence)
@@ -70,14 +68,10 @@ namespace YAT.Domain
 		/// </summary>
 		public virtual void Reset()
 		{
-			PendingMultiBytesToDecode                  = new List<byte>(4); // Preset the required capacity to improve memory management; 4 is the maximum value for multi-byte characters.
-
-			ShownCharCount                             = 0;
-
-			EolOfGivenDevice                           = new Dictionary<string, SequenceQueue>(); // No preset needed, the default initial capacity is good enough.
-			EolOfLastLineOfGivenDeviceWasCompleteMatch = new Dictionary<string, bool>();          // No preset needed, the default initial capacity is good enough.
-
-			RetainedUnconfirmedHiddenEolElements       = new DisplayElementCollection(DisplayElementCollection.TypicalNumberOfElementsPerLine); // Preset the typical capacity to improve memory management.
+			PendingMultiBytesToDecode            = new List<byte>(4); // Preset the required capacity to improve memory management; 4 is the maximum value for multi-byte characters.
+			ShownCharCount                       = 0;
+			EolOfGivenDevice                     = new Dictionary<string, SequenceQueue>(); // No preset needed, the default initial capacity is good enough.
+			RetainedUnconfirmedHiddenEolElements = new DisplayElementCollection(DisplayElementCollection.TypicalNumberOfElementsPerLine); // Preset the typical capacity to improve memory management.
 		}
 
 		/// <summary>
@@ -91,14 +85,19 @@ namespace YAT.Domain
 		/// <summary>
 		/// Notify the end of a line, i.e. continues processing with the next line.
 		/// </summary>
-		public virtual void NotifyLineEnd(string formerDevice, bool eolWasCompleteMatch)
+		public virtual void NotifyLineEnd(string formerDevice)
 		{
 			ShownCharCount = 0;
 
+			var eolOfGivenDeviceIsCompleteMatch = false;
+
 			if (EolOfGivenDevice.ContainsKey(formerDevice))
 			{
-				if (EolOfGivenDevice[formerDevice].IsCompleteMatch)
+				if (EolOfGivenDevice[formerDevice].IsCompleteMatch) {
 					EolOfGivenDevice[formerDevice].Reset();
+
+					eolOfGivenDeviceIsCompleteMatch = true;
+				}
 
 				// Keep EOL state when incomplete. Subsequent lines
 				// need this to handle broken/pending EOL characters.
@@ -108,22 +107,8 @@ namespace YAT.Domain
 				EolOfGivenDevice.Add(formerDevice, new SequenceQueue(EolSequence)); // since there is only a given number of devices.
 			}                                                                       // Applies to TCP and UDP terminals only.
 
-			if (eolWasCompleteMatch) // Keep unconfirmed hidden elements! They shall be delay-shown in case EOL is indeed unconfirmed!
+			if (eolOfGivenDeviceIsCompleteMatch) // Otherwise keep unconfirmed hidden elements! They shall be delay-shown in case EOL is indeed unconfirmed!
 				RetainedUnconfirmedHiddenEolElements = new DisplayElementCollection(); // No preset needed, the default initial capacity is good enough.
-
-			if (EolOfLastLineOfGivenDeviceWasCompleteMatch.ContainsKey(formerDevice))
-				EolOfLastLineOfGivenDeviceWasCompleteMatch[formerDevice] = eolWasCompleteMatch;
-			else
-				EolOfLastLineOfGivenDeviceWasCompleteMatch.Add(formerDevice, eolWasCompleteMatch); // Same as above, it is OK to only access or add to the collection.
-		}
-
-		/// <summary></summary>
-		public virtual bool EolOfLastLineWasCompleteMatch(string dev)
-		{
-			if (EolOfLastLineOfGivenDeviceWasCompleteMatch.ContainsKey(dev))
-				return (EolOfLastLineOfGivenDeviceWasCompleteMatch[dev]);
-			else
-				return (true); // Cleared monitors mean that last line was complete!
 		}
 
 		/// <summary></summary>
