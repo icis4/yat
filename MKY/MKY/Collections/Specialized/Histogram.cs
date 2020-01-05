a20663976d5839aeb8c2633d8e764f99a9d273ab
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2020-01-05 21:14:05 +0000

    Histogram continued

--- MKY/MKY/Collections/Specialized/Histogram.cs
+++ MKY/MKY/Collections/Specialized/Histogram.cs
@@ -32,74 +32,96 @@ namespace MKY.Collections.Specialized
 	/// </summary>
 	/// <typeparam name="T">The type of the values of the histogram</typeparam>
 	public abstract class Histogram<T>
+		where T : IComparable<T>
 	{
 		/// <summary></summary>
-		public const int DefaultBinCount = 100;
+		public const int DefaultBinCount = 101;
 
 		/// <summary></summary>
-		public readonly bool AutoResize; // = false;
+		public readonly int MaxBinCount; // Must be initialized with up to 'int.MaxValue'.
 
 		/// <summary></summary>
-		public readonly int MaxBinCount;
+		public readonly bool AutoRearrange; // = false;
 
-		private List<long> values = new List<long>(); // No preset needed, the default behavior is good enough.
-		private List<long> bins; // = null;
+		/// <summary></summary>
+		public T Min { get; protected set; } // = default(T);
+
+		/// <summary></summary>
+		public T Max { get; protected set; } // = default(T);
+
+		/// <summary></summary>
+		public T BinSize { get; protected set; } // = default(T);
+
+		/// <summary></summary>
+		protected List<T> Values = new List<T>(); // No preset needed, the default behavior is good enough.
+
+		/// <summary></summary>
+		protected List<long> Bins; // = null;
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="Histogram{T}"/> class with a <see cref="BinCount"/>
-		/// of <see cref="DefaultBinCount"/>.
+		/// Initializes a new instance of the <see cref="Histogram{T}"/> class with equally
+		/// distributed bins, fixed to the given arguments.
 		/// </summary>
-		public Histogram()
-			: this(DefaultBinCount)
+		public Histogram(T min, T max, int binCount)
 		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Histogram{T}"/> class.
-		/// </summary>
-		public Histogram(int binCount)
-		{
-			AutoResize = false;
+			Min = min;
+			Max = max;
 			MaxBinCount = binCount;
 			InitializeBins(binCount);
+			AutoRearrange = false;
 		}
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="Histogram{T}"/> class,
-		/// automatically resizing up to <paramref name="maxBinCount"/>.
+		/// Initializes a new instance of the <see cref="Histogram{T}"/> class with equally
+		/// distributed bins, automatically rearranging up to <paramref name="maxBinCount"/>.
 		/// </summary>
 		public Histogram(int initialBinCount, int maxBinCount)
 		{
-			AutoResize = true;
 			MaxBinCount = maxBinCount;
 			InitializeBins(initialBinCount);
+			AutoRearrange = true;
 		}
 
 		private void InitializeBins(int binCount)
 		{
-			this.bins = new List<long>(binCount);
+			Bins = new List<long>(binCount);
 			for (int i = 0; i < binCount; i++)
-				this.bins.Add(0);
+				Bins.Add(0);
 		}
 
 		/// <summary>
 		/// Gets the current bin count.
 		/// </summary>
-		public int BinCount
+		public virtual int BinCount
 		{
-			get { return (this.bins.Count); }
+			get { return (Bins.Count); }
 		}
+
+		/// <summary>
+		/// Gets the index of the bin corresponding to <paramref name="value"/>.
+		/// </summary>
+		protected abstract int GetIndex(T value);
 
 		/// <summary>
 		/// Adds an item to the histogram.
 		/// </summary>
-		public abstract void Add(T item);
+		public virtual void Add(T item)
+		{
+			Values.Add(item);
+
+			if ((item.CompareTo(Min) >= 0) && (item.CompareTo(Max) <= 0))
+				Bins[GetIndex(item)]++;
+			else if (AutoRearrange)
+				Rearrange();
+			else
+				throw (new ArgumentOutOfRangeException("item", item, "The value of the item is outside 'Min'/'Max' and 'AutoRearrange' is 'false'!"));
+		}
 
 		/// <summary>
-		/// Resizes the collection up to <see cref="MaxBinCount"/>.
-		/// Needed when <see cref="AutoResize"/> is <c>true</c>.
+		/// Rearranges the collection up to <see cref="MaxBinCount"/>.
+		/// Needed when <see cref="AutoRearrange"/> is <c>true</c>.
 		/// </summary>
-		protected abstract void Resize();
+		protected abstract void Rearrange();
 	}
 
 	/// <summary>
@@ -107,12 +129,45 @@ namespace MKY.Collections.Specialized
 	/// </summary>
 	public class HistogramDouble : Histogram<double>
 	{
-		public override void Add(double item)
+		/// <summary></summary>
+		public const double DefaultMin = -0.5;
+
+		/// <summary></summary>
+		public const double DefaultMax = 100.5;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="HistogramDouble"/> class with equally
+		/// distributed bins, fixed to the given arguments.
+		/// </summary>
+		public HistogramDouble(double min = DefaultMin, double max = DefaultMax, int binCount = DefaultBinCount)
+			: base(min, max, binCount)
+		{
+			BinSize = ((max - min) / binCount);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="HistogramDouble"/> class with equally
+		/// distributed bins, automatically rearranging up to <paramref name="maxBinCount"/>.
+		/// </summary>
+		public HistogramDouble(int initialBinCount, int maxBinCount)
+			: base(initialBinCount, maxBinCount)
+		{
+		////BinSize = 0.0 = default(T);
+		}
+
+		/// <summary>
+		/// Gets the index of the bin corresponding to <paramref name="value"/>.
+		/// </summary>
+		protected override int GetIndex(double value)
 		{
 			throw new NotImplementedException();
 		}
 
-		protected override void Resize()
+		/// <summary>
+		/// Rearranges the collection up to <see cref="Histogram{T}.MaxBinCount"/>.
+		/// Needed when <see cref="Histogram{T}.AutoRearrange"/> is <c>true</c>.
+		/// </summary>
+		protected override void Rearrange()
 		{
 			throw new NotImplementedException();
 		}
