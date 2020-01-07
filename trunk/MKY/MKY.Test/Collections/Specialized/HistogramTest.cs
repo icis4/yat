c930531b78894689eb3c8bc0494b9f3f2f2e8fc9
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2020-01-07 14:42:36 +0000

    Histogram completed

--- trunk/MKY/MKY.Test/Collections/Specialized/HistogramTest.cs
+++ trunk/MKY/MKY.Test/Collections/Specialized/HistogramTest.cs
@@ -44,73 +44,131 @@ namespace MKY.Test.Collections.Specialized
 			Assert.That(histogram.Max, Is.EqualTo(HistogramDouble.DefaultMax));
 			Assert.That(histogram.BinCount, Is.EqualTo(HistogramDouble.DefaultBinCount));
 			Assert.That(histogram.BinSize, Is.GreaterThan(0.0));
-			Assert.That(histogram.ValueCount, Is.EqualTo(0));
+			Assert.That(histogram.ItemCount, Is.EqualTo(0));
 
 			histogram.AddRange(new double[] { -100.0, -99.99, -0.01, 0.0, 0.01, 99.99, 100.0 });
+			var lowIndex = 0;
+			var midIndex = (int)Math.Ceiling((double)(HistogramDouble.DefaultBinCount - 1) / 2);
+			var highIndex = (HistogramDouble.DefaultBinCount - 1);
 			Assert.That(histogram.Min, Is.EqualTo(HistogramDouble.DefaultMin));
 			Assert.That(histogram.Max, Is.EqualTo(HistogramDouble.DefaultMax));
 			Assert.That(histogram.BinCount, Is.EqualTo(HistogramDouble.DefaultBinCount));
 			Assert.That(histogram.BinSize, Is.GreaterThan(0.0));
-			Assert.That(histogram.ValueCount, Is.EqualTo(7));
+			Assert.That(histogram.ItemCount, Is.EqualTo(7));
 			Assert.That(histogram.Counts.Count, Is.EqualTo(HistogramDouble.DefaultBinCount));
-			Assert.That(histogram.Counts[0], Is.EqualTo(2));
-			Assert.That(histogram.Counts[(int)Math.Ceiling((double)HistogramDouble.DefaultBinCount / 2)], Is.EqualTo(3));
-			Assert.That(histogram.Counts[HistogramDouble.DefaultBinCount], Is.EqualTo(2));
+			Assert.That(histogram.Counts[lowIndex],  Is.EqualTo(2));
+			Assert.That(histogram.Counts[midIndex],  Is.EqualTo(3));
+			Assert.That(histogram.Counts[highIndex], Is.EqualTo(2));
 		}
 
 		/// <summary></summary>
 		[Test]
-		public virtual void TestHistogramDoubleAutoRearrange()
+		public virtual void TestHistogramDoubleAutoGrow()
 		{
-			const int MaxBinCount = 256;
+			const int MaxBinCount = 256; // Proximity for ~10.0 is +/- 0.1953, i.e. 0.01 is sufficient.
+			const double DeltaWithinProximity = 0.01;
 
-			var histogram = new HistogramDouble(MaxBinCount);
-			Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
-			Assert.That(histogram.Max, Is.EqualTo(0.0).Within(Few).Ulps);
-			Assert.That(histogram.BinCount, Is.EqualTo(0));
-			Assert.That(histogram.BinSize, Is.EqualTo(0.0).Within(Few).Ulps);
-			Assert.That(histogram.ValueCount, Is.EqualTo(0));
+			// Immediately adding proximate values:
+			{
+				var histogram = new HistogramDouble(MaxBinCount);
+				Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.Max, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.BinCount, Is.EqualTo(1));
+				Assert.That(histogram.BinSize, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.ItemCount, Is.EqualTo(0));
 
-			histogram.Add(0.0);
-			Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
-			Assert.That(histogram.Max, Is.EqualTo(0.0).Within(Few).Ulps);
-			Assert.That(histogram.BinCount, Is.EqualTo(1));
-			Assert.That(histogram.BinSize, Is.EqualTo(0.0).Within(Few).Ulps);
-			Assert.That(histogram.ValueCount, Is.EqualTo(1));
-			Assert.That(histogram.Counts.Count, Is.EqualTo(1));
-			Assert.That(histogram.Counts[0], Is.EqualTo(1));
+				histogram.Add(0.0);
+				Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.Max, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.BinCount, Is.EqualTo(1));
+				Assert.That(histogram.BinSize, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.ItemCount, Is.EqualTo(1));
+				Assert.That(histogram.Counts.Count, Is.EqualTo(1));
+				Assert.That(histogram.Counts[0], Is.EqualTo(1));
 
-			histogram.Add(10.0);
-			Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
-			Assert.That(histogram.Max, Is.EqualTo(10.0).Within(Few).Ulps);
-			Assert.That(histogram.BinCount, Is.EqualTo(2));
-			Assert.That(histogram.BinSize, Is.EqualTo(5.0).Within(Few).Ulps);
-			Assert.That(histogram.ValueCount, Is.EqualTo(2));
-			Assert.That(histogram.Counts.Count, Is.EqualTo(2));
-			Assert.That(histogram.Counts[0], Is.EqualTo(1));
-			Assert.That(histogram.Counts[1], Is.EqualTo(1));
+				histogram.Add(0.0 + DeltaWithinProximity);
+				Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.Max, Is.EqualTo(0.0 + DeltaWithinProximity).Within(Few).Ulps);
+				Assert.That(histogram.BinCount, Is.EqualTo(2));
+				Assert.That(histogram.BinSize, Is.EqualTo(DeltaWithinProximity / 2).Within(Few).Ulps);
+				Assert.That(histogram.ItemCount, Is.EqualTo(2));
+				Assert.That(histogram.Counts.Count, Is.EqualTo(2));
+				Assert.That(histogram.Counts[0], Is.EqualTo(1));
+				Assert.That(histogram.Counts[1], Is.EqualTo(1));
+			}
 
-			histogram.Add(20.0);
-			Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
-			Assert.That(histogram.Max, Is.EqualTo(20.0).Within(Few).Ulps);
-			Assert.That(histogram.BinCount, Is.EqualTo(4));
-			Assert.That(histogram.BinSize, Is.EqualTo(5.0).Within(Few).Ulps);
-			Assert.That(histogram.ValueCount, Is.EqualTo(3));
-			Assert.That(histogram.Counts.Count, Is.EqualTo(3));
-			Assert.That(histogram.Counts[0], Is.EqualTo(1));
-			Assert.That(histogram.Counts[1], Is.EqualTo(1));
-			Assert.That(histogram.Counts[3], Is.EqualTo(1));
+			// Later adding proximate values:
+			{
+				var histogram = new HistogramDouble(MaxBinCount);
+				Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.Max, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.BinCount, Is.EqualTo(1));
+				Assert.That(histogram.BinSize, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.ItemCount, Is.EqualTo(0));
 
-			histogram.Add(20.0);
-			Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
-			Assert.That(histogram.Max, Is.EqualTo(20.0).Within(Few).Ulps);
-			Assert.That(histogram.BinCount, Is.EqualTo(4));
-			Assert.That(histogram.BinSize, Is.EqualTo(5.0).Within(Few).Ulps);
-			Assert.That(histogram.ValueCount, Is.EqualTo(4));
-			Assert.That(histogram.Counts.Count, Is.EqualTo(4));
-			Assert.That(histogram.Counts[0], Is.EqualTo(1));
-			Assert.That(histogram.Counts[1], Is.EqualTo(1));
-			Assert.That(histogram.Counts[3], Is.EqualTo(2));
+				histogram.Add(0.0);
+				Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.Max, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.BinCount, Is.EqualTo(1));
+				Assert.That(histogram.BinSize, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.ItemCount, Is.EqualTo(1));
+				Assert.That(histogram.Counts.Count, Is.EqualTo(1));
+				Assert.That(histogram.Counts[0], Is.EqualTo(1));
+
+				histogram.Add(10.0);
+				Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.Max, Is.EqualTo(10.0).Within(Few).Ulps);
+				Assert.That(histogram.BinCount, Is.EqualTo(2));
+				Assert.That(histogram.BinSize, Is.EqualTo(5.0).Within(Few).Ulps);
+				Assert.That(histogram.ItemCount, Is.EqualTo(2));
+				Assert.That(histogram.Counts.Count, Is.EqualTo(2));
+				Assert.That(histogram.Counts[0], Is.EqualTo(1));
+				Assert.That(histogram.Counts[1], Is.EqualTo(1));
+
+				histogram.Add(10.0 + DeltaWithinProximity); // = in proximity of item
+				Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.Max, Is.EqualTo(10.0 + DeltaWithinProximity).Within(Few).Ulps);
+				Assert.That(histogram.BinCount, Is.EqualTo(2));
+				Assert.That(histogram.BinSize, Is.EqualTo(5.0).Within(Few).Ulps);
+				Assert.That(histogram.ItemCount, Is.EqualTo(3));
+				Assert.That(histogram.Counts.Count, Is.EqualTo(2));
+				Assert.That(histogram.Counts[0], Is.EqualTo(1));
+				Assert.That(histogram.Counts[1], Is.EqualTo(2));
+
+				histogram.Add(20.0);
+				Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.Max, Is.EqualTo(20.0).Within(Few).Ulps);
+				Assert.That(histogram.BinCount, Is.EqualTo(4));
+				Assert.That(histogram.BinSize, Is.EqualTo(5.0).Within(Few).Ulps);
+				Assert.That(histogram.ItemCount, Is.EqualTo(4));
+				Assert.That(histogram.Counts.Count, Is.EqualTo(4));
+				Assert.That(histogram.Counts[0], Is.EqualTo(1));
+				Assert.That(histogram.Counts[1], Is.EqualTo(2));
+				Assert.That(histogram.Counts[3], Is.EqualTo(1));
+
+				histogram.Add(20.0 - DeltaWithinProximity); // = in proximity of item
+				Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.Max, Is.EqualTo(20.0).Within(Few).Ulps);
+				Assert.That(histogram.BinCount, Is.EqualTo(4));
+				Assert.That(histogram.BinSize, Is.EqualTo(5.0).Within(Few).Ulps);
+				Assert.That(histogram.ItemCount, Is.EqualTo(5));
+				Assert.That(histogram.Counts.Count, Is.EqualTo(4));
+				Assert.That(histogram.Counts[0], Is.EqualTo(1));
+				Assert.That(histogram.Counts[1], Is.EqualTo(2));
+				Assert.That(histogram.Counts[3], Is.EqualTo(2));
+
+				histogram.Add(12.5 + DeltaWithinProximity); // = in proximity of bin value
+				Assert.That(histogram.Min, Is.EqualTo(0.0).Within(Few).Ulps);
+				Assert.That(histogram.Max, Is.EqualTo(20.0).Within(Few).Ulps);
+				Assert.That(histogram.BinCount, Is.EqualTo(4));
+				Assert.That(histogram.BinSize, Is.EqualTo(5.0).Within(Few).Ulps);
+				Assert.That(histogram.ItemCount, Is.EqualTo(6));
+				Assert.That(histogram.Counts.Count, Is.EqualTo(4));
+				Assert.That(histogram.Counts[0], Is.EqualTo(1));
+				Assert.That(histogram.Counts[1], Is.EqualTo(2));
+				Assert.That(histogram.Counts[2], Is.EqualTo(1));
+				Assert.That(histogram.Counts[3], Is.EqualTo(2));
+			}
 		}
 	}
 }
