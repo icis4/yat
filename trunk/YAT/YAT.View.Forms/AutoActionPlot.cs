fdfab9ed1da02966848da01a062fe7aef18fa521
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2020-01-10 10:45:39 +0000

    Histogram changed to PlotBar()

--- trunk/YAT/YAT.View.Forms/AutoActionPlot.cs
+++ trunk/YAT/YAT.View.Forms/AutoActionPlot.cs
@@ -463,22 +463,7 @@ namespace YAT.View.Forms
 		{
 			if (mdl.Histogram != null)
 			{
-				// Account for the fact that 'Plot.PlotStep()' requires additional first/last points.
-				// Pretty inefficient, but considered good enough for the moment.
-
-				var xs = mdl.Histogram.ValuesLowerLimit;
-				var xsFramed = new List<double>(xs.Count + 2);
-				xsFramed.Add(xs.First()); // Begin first step.
-				xsFramed.AddRange(xs);
-				xsFramed.Add(xs.Last()); // End last step.
-
-				var ys = mdl.Histogram.Counts;
-				var ysFramed = new List<long>(ys.Count + 2);
-				ysFramed.Add(0); // Begin first step.
-				ysFramed.AddRange(ys);
-				ysFramed.Add(0); // End last step.
-
-				scottPlot.plt.PlotStep(xsFramed.ToArray(), ysFramed.Select(x => (double)x).ToArray(), color: rxColor, label: "All Captures");
+				scottPlot.plt.PlotBar(mdl.Histogram.ValuesLowerLimit.ToArray(), mdl.Histogram.Counts.Select(x => (double)x).ToArray(), barWidth: mdl.Histogram.BinSize, color: rxColor, label: "All Captures");
 			}
 		}
 
