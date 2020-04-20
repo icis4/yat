5b2255deb9e31fbfbb3854913dac88622ba9c686
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2020-04-20 19:58:51 +0000

    Rate fixed

--- trunk/MKY/MKY/Time/Rate.cs
+++ trunk/MKY/MKY/Time/Rate.cs
@@ -35,8 +35,9 @@ namespace MKY.Time
 		// Fields
 		//==========================================================================================
 
-		private int interval;
 		private int window;
+		private int interval;
+		private int numberOfIntervals;
 
 		private Queue<TimeStampItem<int>> queue;
 		private int value;
@@ -54,19 +55,25 @@ namespace MKY.Time
 		{
 		}
 
-		/// <summary></summary>
+		/// <param name="interval">The interval to calculate the value of the rate</param>
+		/// <remarks><see cref="Window"/> will be set to <paramref name="interval"/>.</remarks>
 		public Rate(int interval)
 			: this(interval, interval)
 		{
 		}
 
-		/// <summary></summary>
+		/// <param name="interval">The interval to calculate the value of the rate.</param>
+		/// <param name="window"><see cref="Window"/>.</param>
 		public Rate(int interval, int window)
 		{
+			if (window < interval)
+				throw (new ArgumentOutOfRangeException("window", window, MessageHelper.InvalidExecutionPreamble + "Value must be equal or larger than 'interval' = '" + interval + "'!" + Environment.NewLine + Environment.NewLine + MessageHelper.SubmitBug));
+
 			this.interval = interval;
 			this.window   = window;
+			UpdateNumberOfIntervals();
 
-			this.queue = new Queue<TimeStampItem<int>>(window); // Preset the assumed capacity to improve memory management.
+			this.queue = new Queue<TimeStampItem<int>>(); // No clue how many items to expect, the default behavior must be good enough.
 		}
 
 		#endregion
@@ -77,21 +84,40 @@ namespace MKY.Time
 		//==========================================================================================
 
 		/// <summary>
-		/// The interval to evaluate partial values of the rate.
+		/// The interval to calculate the value of the rate.
 		/// </summary>
 		public int Interval
 		{
 			get { return (this.interval); }
-			set { this.interval = value;  }
+			set
+			{
+				if (value > window)
+					throw (new ArgumentOutOfRangeException("value", value, MessageHelper.InvalidExecutionPreamble + "Value must be equal or less than 'window' = '" + window + "'!" + Environment.NewLine + Environment.NewLine + MessageHelper.SubmitBug));
+
+				this.interval = value;
+				UpdateNumberOfIntervals();
+			}
 		}
 
 		/// <summary>
-		/// The total window to evaluate the rate.
+		/// The window to calculate the rate.
 		/// </summary>
+		/// <remarks>
+		/// If window is larger than <see cref="Interval"/>, a value is calculated for each interval
+		/// and the values are weighed. The value of the less recent interval is weighed most, more
+		/// recent intervals are weighed less.
+		/// </remarks>
 		public int Window
 		{
 			get { return (this.window); }
-			set { this.window = value;  }
+			set
+			{
+				if (value < interval)
+					throw (new ArgumentOutOfRangeException("value", value, MessageHelper.InvalidExecutionPreamble + "Value must be equal or larger than 'interval' = '" + interval + "'!" + Environment.NewLine + Environment.NewLine + MessageHelper.SubmitBug));
+
+				this.window = value;
+				UpdateNumberOfIntervals();
+			}
 		}
 
 		/// <summary>
@@ -111,25 +137,40 @@ namespace MKY.Time
 		//==========================================================================================
 
 		/// <summary></summary>
-		public virtual bool Update(int value)
+		public virtual bool Update(int item)
 		{
-			AddValueToQueue(value);
-			RemoveObsoleteFromQueue();
-			return (CalculateValueFromQueue(value));
+			int newValue;
+			return (Update(item, out newValue));
 		}
 
 		/// <summary></summary>
-		public virtual bool Update(DateTime endOfWindow)
+		public virtual bool Update(int item, out int newValue)
 		{
-			RemoveObsoleteFromQueue(endOfWindow);
-			return (CalculateValueFromQueue());
+			var now = DateTime.Now; // A single time stamp for the whole operation.
+			AddItemToQueue(now, item);
+			RemoveObsoleteItemsFromQueue(now);
+			return (CalculateValueFromQueue(now, out newValue));
+		}
+
+		/// <summary></summary>
+		public virtual bool Update(DateTime timeStamp)
+		{
+			int newValue;
+			return (Update(timeStamp, out newValue));
+		}
+
+		/// <summary></summary>
+		public virtual bool Update(DateTime timeStamp, out int newValue)
+		{
+			RemoveObsoleteItemsFromQueue(timeStamp);
+			return (CalculateValueFromQueue(timeStamp, out newValue));
 		}
 
 		/// <summary></summary>
 		public virtual void Reset()
 		{
 			ClearQueue();
-			CalculateValueFromQueue();
+			ClearValue();
 		}
 
 		#region Methods > Private
@@ -137,24 +178,24 @@ namespace MKY.Time
 		// Methods > Private
 		//------------------------------------------------------------------------------------------
 
+		private void UpdateNumberOfIntervals()
+		{
+			this.numberOfIntervals = (int)(Math.Ceiling((double)this.window / this.interval));
+		}
+
 		private void ClearQueue()
 		{
 			lock (this.queue) // Lock is required because Queue<T> is not synchronized and whole queue is accessed via ToArray().
 				this.queue.Clear();
 		}
 
-		private void AddValueToQueue(int value)
+		private void AddItemToQueue(DateTime timeStamp, int item)
 		{
 			lock (this.queue) // Lock is required because Queue<T> is not synchronized and whole queue is accessed via ToArray().
-				this.queue.Enqueue(new TimeStampItem<int>(value));
+				this.queue.Enqueue(new TimeStampItem<int>(timeStamp, item));
 		}
 
-		private void RemoveObsoleteFromQueue()
-		{
-			RemoveObsoleteFromQueue(DateTime.Now);
-		}
-
-		private void RemoveObsoleteFromQueue(DateTime endOfWindow)
+		private void RemoveObsoleteItemsFromQueue(DateTime endOfWindow)
 		{
 			DateTime beginningOfWindow = (endOfWindow - TimeSpan.FromMilliseconds(this.window));
 
@@ -171,53 +212,42 @@ namespace MKY.Time
 			}
 		}
 
-		private bool CalculateValueFromQueue(int value = 0)
+		private bool CalculateValueFromQueue(DateTime timeStamp, out int newValue)
 		{
-			int oldValue = this.value;
-			int newValue = 0;
+			// Get items from queue:
+			TimeStampItem<int>[] qa;
+			lock (this.queue) // Lock is required because Queue<T> is not synchronized and whole queue is accessed via ToArray().
+				qa = this.queue.ToArray();
 
-			// If value was 0 before, only consider the current value:
-			if (oldValue <= 0)
+			// Prepare calculation:
+			int[] valuePerInterval = ArrayEx.CreateAndInitializeInstance(this.numberOfIntervals, 0); // Makes no sense to keep as member.
+			                                                                                       //// Requires initialization to 0 anyway.
+			unchecked
 			{
-				newValue = value;
-			}
-			else
-			{
-				unchecked
+				// Count number of items within each interval:
+				foreach (TimeStampItem<int> tsi in qa) // Queue will only contain items within the window.
 				{
-					// Count number of items within each interval:
-					int numberOfIntervals = (this.window / this.interval);
-					int[] valuePerInterval = ArrayEx.CreateAndInitializeInstance(numberOfIntervals, 0);
-					DateTime now = DateTime.Now;
-
-					TimeStampItem<int>[] qa;
-					lock (this.queue) // Lock is required because Queue<T> is not synchronized and whole queue is accessed via ToArray().
-						qa = this.queue.ToArray();
-
-					foreach (TimeStampItem<int> tsi in qa)
-					{
-						TimeSpan ts = (now - tsi.TimeStamp);
-						int i = Int32Ex.Limit((int)(ts.TotalMilliseconds / this.interval), 0, Math.Max((numberOfIntervals - 1), 0)); // 'max' must be 0 or above.
-						valuePerInterval[i] += tsi.Item;
-					}
-
-					// Weigh and sum up the intervals:
-					int weight = numberOfIntervals;
-					int weighedSum = 0;
-					int sumOfWeights = 0;
-					foreach (int valueOfInterval in valuePerInterval)
-					{
-						weighedSum += (valueOfInterval * weight);
-						sumOfWeights += weight;
-						weight--;
-					}
-
-					// Evaluate the rate:
-					newValue = (int)(Math.Round((double)weighedSum / sumOfWeights));
+					TimeSpan ts = (timeStamp - tsi.TimeStamp);
+					int i = Int32Ex.Limit((int)(Math.Round(ts.TotalMilliseconds / this.interval)), 0, (valuePerInterval.Length - 1));
+					valuePerInterval[i] += tsi.Item;
 				}
+
+				// Weigh and sum up the intervals:
+				int weight = this.numberOfIntervals;
+				int weighedSum = 0;
+				int sumOfWeights = 0;
+				foreach (int valueOfInterval in valuePerInterval)
+				{
+					weighedSum += (valueOfInterval * weight);
+					sumOfWeights += weight;
+					weight--;
+				}
+
+				// Evaluate the rate:
+				newValue = (int)(Math.Round((double)weighedSum / sumOfWeights));
 			}
 
-			if (newValue != oldValue)
+			if (this.value != newValue)
 			{
 				this.value = newValue;
 				return (true);
@@ -226,6 +256,11 @@ namespace MKY.Time
 			{
 				return (false);
 			}
+		}
+
+		private void ClearValue()
+		{
+			this.value = 0;
 		}
 
 		#endregion
