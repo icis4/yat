cd8aa7b652ebed974d9add8b95b5654ba7c53ab9
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2020-04-28 09:10:11 +0000

    CommandTest extended (related to bug #481)

--- trunk/YAT/YAT.Model.Test/CommandTest.cs
+++ trunk/YAT/YAT.Model.Test/CommandTest.cs
@@ -39,36 +39,51 @@ namespace YAT.Model.Test
 		{
 			var c = new Command();
 
-			const string Text = "ABC";
-			c.SingleLineText = Text;
-			AssertSingleLineText(c, Text);
+			string text = "ABC";
+			c.SingleLineText = text;
+			AssertSingleLineText(c, text);
 
-			const string Description = "XYZ";
-			c.Description = Description;
-			AssertSingleLineText(c, Text, Description);
+			string description = "XYZ";
+			c.Description = description;
+			AssertSingleLineText(c, text, description);
 		}
 
 		/// <summary></summary>
 		[Test]
 		public virtual void TestInitialSingleLineTextThenModify()
 		{
-			const string InitialText = "ABC";
-			const string InitialDescription = "XYZ";
-			var c = new Command(InitialDescription, InitialText);
-			AssertSingleLineText(c, InitialText, InitialDescription);
+			string initialText = "ABC";
+			var c = new Command(initialText);
+			AssertSingleLineText(c, initialText);
 
-			const string ModifiedDescription = "uvw";
-			c.Description = ModifiedDescription;
-			AssertSingleLineText(c, InitialText, ModifiedDescription);
+			string modifiedText = "abc"; // Change casing.
+			c.SingleLineText = modifiedText;
+			AssertSingleLineText(c, modifiedText);
 
-			const string ModifiedText = "def";
-			c.SingleLineText = ModifiedText;
-			AssertSingleLineText(c, ModifiedText, ModifiedDescription);
+			modifiedText = "abcdef"; // Add text.
+			c.SingleLineText = modifiedText;
+			AssertSingleLineText(c, modifiedText);
+
+			modifiedText = "abef"; // Remove text.
+			c.SingleLineText = modifiedText;
+			AssertSingleLineText(c, modifiedText);
+
+			string description = "XYZ";
+			c.Description = description;
+			AssertSingleLineText(c, modifiedText, description);
+
+			modifiedText = "abc";
+			c.SingleLineText = modifiedText;
+			AssertSingleLineText(c, modifiedText, description);
+
+			modifiedText = "ABC";
+			c.SingleLineText = modifiedText;
+			AssertSingleLineText(c, modifiedText, description);
 		}
 
 		/// <summary></summary>
 		[SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Default parameters may result in cleaner code and clearly indicate the default behavior.")]
-		public virtual void AssertSingleLineText(Command c, string text, string description = null, Domain.Parser.Mode modes = Domain.Parser.Mode.NoEscapes)
+		protected virtual void AssertSingleLineText(Command c, string text, string description = null, Domain.Parser.Mode modes = Domain.Parser.Mode.Default)
 		{
 			Assert.That(c.IsDefined,          Is.True);
 			Assert.That(c.IsValid(modes),     Is.True);
@@ -92,6 +107,86 @@ namespace YAT.Model.Test
 
 			if (string.IsNullOrEmpty(description))
 				Assert.That(c.Description,    Is.EqualTo(text));
+			else
+				Assert.That(c.Description,    Is.EqualTo(description));
+		}
+
+		/// <summary></summary>
+		[Test]
+		public virtual void TestEmptyThenMultiLineText()
+		{
+			var c = new Command();
+
+			string[] text = new string[]{ "ABC", "DEF" };
+			string singleLineText = "<2 lines...> [ABC] [DEF]";
+			c.MultiLineText = text;
+			AssertMultiLineText(c, text, singleLineText);
+
+			string description = "XYZ";
+			c.Description = description;
+			AssertMultiLineText(c, text, singleLineText, description);
+		}
+
+		/// <summary></summary>
+		[Test]
+		public virtual void TestInitialMultiLineTextThenModify()
+		{
+			string[] initialMulti = new string[]{ "ABC", "DEF" };
+			string initialSingle = "<2 lines...> [ABC] [DEF]";
+			var c = new Command(initialMulti);
+			AssertMultiLineText(c, initialMulti, initialSingle);
+
+			string[] modifiedMulti = new string[]{ "ABC", "def" }; // Change casing.
+			string modifiedSingle = "<2 lines...> [ABC] [def]";
+			c.MultiLineText = modifiedMulti;
+			AssertMultiLineText(c, modifiedMulti, modifiedSingle);
+
+			modifiedMulti = new string[]{ "ABC", "def", "ghi" }; // Add a line.
+			modifiedSingle = "<3 lines...> [ABC] [def] [ghi]";
+			c.MultiLineText = modifiedMulti;
+			AssertMultiLineText(c, modifiedMulti, modifiedSingle);
+
+			modifiedMulti = new string[]{ "ABC", "ghi" }; // Remove a line.
+			modifiedSingle = "<2 lines...> [ABC] [ghi]";
+			c.MultiLineText = modifiedMulti;
+			AssertMultiLineText(c, modifiedMulti, modifiedSingle);
+
+			string description = "XYZ";
+			c.Description = description;
+			AssertMultiLineText(c, modifiedMulti, modifiedSingle, description);
+
+			modifiedMulti = new string[]{ "ABC", "DEF" };
+			modifiedSingle = "<2 lines...> [ABC] [DEF]";
+			c.MultiLineText = modifiedMulti;
+			AssertMultiLineText(c, modifiedMulti, modifiedSingle, description);
+		}
+
+		/// <summary></summary>
+		[SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Default parameters may result in cleaner code and clearly indicate the default behavior.")]
+		protected virtual void AssertMultiLineText(Command c, string[] multiLineText, string singleLineText, string description = null, Domain.Parser.Mode modes = Domain.Parser.Mode.Default)
+		{
+			Assert.That(c.IsDefined,          Is.True);
+			Assert.That(c.IsValid(modes),     Is.True);
+			Assert.That(c.Caption,            Is.EqualTo(singleLineText));
+
+			Assert.That(c.IsText,             Is.True);
+			Assert.That(c.IsValidText(modes), Is.True);
+			Assert.That(c.IsSingleLineText,   Is.False);
+			Assert.That(c.SingleLineText,     Is.EqualTo(singleLineText));
+
+			Assert.That(c.IsPartialTextEol,   Is.False);
+			Assert.That(c.IsPartialText,      Is.False);
+			Assert.That(c.PartialText,        Is.EqualTo(singleLineText));
+
+			Assert.That(c.IsMultiLineText,    Is.True);
+			Assert.That(c.MultiLineText,      Is.EqualTo(multiLineText));
+
+			Assert.That(c.IsFilePath,         Is.False);
+			Assert.That(c.IsValidFilePath(),  Is.False);
+			Assert.That(c.FilePath,           Is.Null.Or.Empty);
+
+			if (string.IsNullOrEmpty(description))
+				Assert.That(c.Description,    Is.EqualTo(singleLineText));
 			else
 				Assert.That(c.Description,    Is.EqualTo(description));
 		}
