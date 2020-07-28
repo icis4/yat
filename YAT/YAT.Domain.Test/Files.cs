e7efc1413eddab5c6cecc44ba2fe2c5e4b620660
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2020-07-28 13:04:50 +0000

    Handling of files used for testing simplified

--- YAT/YAT.Domain.Test/Files.cs
+++ YAT/YAT.Domain.Test/Files.cs
@@ -51,7 +51,7 @@ namespace YAT.Domain.Test
 	#pragma warning disable 1591
 
 	/// <summary></summary>
-	public enum StressTestCase
+	public enum StressFile
 	{
 		Normal,
 		Large,
@@ -110,7 +110,7 @@ namespace YAT.Domain.Test
 
 		/// <summary></summary>
 		[SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "Why not?")]
-		public Dictionary<StressTestCase, FileDescriptor> Stress { get; }
+		public Dictionary<StressFile, FileDescriptor> Stress { get; }
 
 		/// <summary></summary>
 		public FileGroup(int capacity)
@@ -132,11 +132,11 @@ namespace YAT.Domain.Test
 			else
 				DirectoryPath = di.FullName + System.IO.Path.DirectorySeparatorChar + "!-TestFiles" + System.IO.Path.DirectorySeparatorChar + directory + System.IO.Path.DirectorySeparatorChar;
 
-			Stress = new Dictionary<StressTestCase, FileDescriptor>(capacity);
+			Stress = new Dictionary<StressFile, FileDescriptor>(capacity);
 		}
 
 		/// <summary></summary>
-		public void Add(StressTestCase fileKey, string fileName, int fileSize, int lineCount)
+		public void Add(StressFile fileKey, string fileName, int fileSize, int lineCount)
 		{
 			Stress.Add(fileKey, new FileDescriptor(DirectoryPath + fileName, fileSize, lineCount));
 		}
@@ -180,23 +180,23 @@ namespace YAT.Domain.Test
 		{
 			// Stress text:        // Preset the required capacity to improve memory management.
 			Text = new FileGroup(9);                                                             // Including EOLs; not including EOF.
-			Text.Add(StressTestCase.Normal,            "Stress-1-Normal.txt",             8400,    300); //  28 bytes per line.
-			Text.Add(StressTestCase.Large,             "Stress-2-Large.txt",             82500,   1500); //  55 bytes per line.
-			Text.Add(StressTestCase.EvenLarger,        "Stress-3-EvenLarger.txt",       275000,   5000); //  55 bytes per line.
-			Text.Add(StressTestCase.Huge,              "Stress-4-Huge.txt",            1090000,  10000); // 109 bytes per line.
-			Text.Add(StressTestCase.Enormous,          "Stress-5-Enormous.txt",       16300000, 100000); // 163 bytes per line.
-			Text.Add(StressTestCase.LongLine,          "Stress-6-LongLine.txt",            973,      1);
-			Text.Add(StressTestCase.VeryLongLine,      "Stress-7-VeryLongLine.txt",       9991,      1);
-			Text.Add(StressTestCase.VeryLongMultiLine, "Stress-8-VeryLongMultiLine.txt", 48650,     50); // 973 bytes per line.
-			Text.Add(StressTestCase.EnormousLine,      "Stress-9-EnormousLine.txt",     500014,      1);
+			Text.Add(StressFile.Normal,            "Stress-1-Normal.txt",             8400,    300); //  28 bytes per line.
+			Text.Add(StressFile.Large,             "Stress-2-Large.txt",             82500,   1500); //  55 bytes per line.
+			Text.Add(StressFile.EvenLarger,        "Stress-3-EvenLarger.txt",       275000,   5000); //  55 bytes per line.
+			Text.Add(StressFile.Huge,              "Stress-4-Huge.txt",            1090000,  10000); // 109 bytes per line.
+			Text.Add(StressFile.Enormous,          "Stress-5-Enormous.txt",       16300000, 100000); // 163 bytes per line.
+			Text.Add(StressFile.LongLine,          "Stress-6-LongLine.txt",            973,      1);
+			Text.Add(StressFile.VeryLongLine,      "Stress-7-VeryLongLine.txt",       9991,      1);
+			Text.Add(StressFile.VeryLongMultiLine, "Stress-8-VeryLongMultiLine.txt", 48650,     50); // 973 bytes per line.
+			Text.Add(StressFile.EnormousLine,      "Stress-9-EnormousLine.txt",     500014,      1);
 
 			// Stress binary:                  // Preset the required capacity to improve memory management.
 			Binary = new FileGroup(5);
-			Binary.Add(StressTestCase.Normal,     "Stress-1-Normal.dat",         8192, -1);
-			Binary.Add(StressTestCase.Large,      "Stress-2-Large.dat",         82432, -1);
-			Binary.Add(StressTestCase.EvenLarger, "Stress-3-EvenLarger.dat",   274944, -1);
-			Binary.Add(StressTestCase.Huge,       "Stress-4-Huge.dat",        1089792, -1);
-			Binary.Add(StressTestCase.Enormous,   "Stress-5-Enormous.dat",   16299776, -1);
+			Binary.Add(StressFile.Normal,     "Stress-1-Normal.dat",         8192, -1);
+			Binary.Add(StressFile.Large,      "Stress-2-Large.dat",         82432, -1);
+			Binary.Add(StressFile.EvenLarger, "Stress-3-EvenLarger.dat",   274944, -1);
+			Binary.Add(StressFile.Huge,       "Stress-4-Huge.dat",        1089792, -1);
+			Binary.Add(StressFile.Enormous,   "Stress-5-Enormous.dat",   16299776, -1);
 		}
 
 		#endregion
