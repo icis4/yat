6b05bdaff14603f7d23f2174c91f81fe233a13c9
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2020-08-07 15:04:11 +0000

    NUnit extension renamed to NUnitEx for clear separation to NUnit

--- trunk/NUnit/NUnitEx/TestCaseDescriptor.cs
+++ trunk/NUnit/NUnitEx/TestCaseDescriptor.cs
@@ -8,35 +8,56 @@
 // $Date$
 // $Author$
 // ------------------------------------------------------------------------------------------------
-// NUnit Version 1.0.22 Development
+// YAT Version 2.2.0 Development
 // ------------------------------------------------------------------------------------------------
 // See release notes for product version details.
 // See SVN change log for file revision details.
 // Author(s): Matthias Klaey
 // ------------------------------------------------------------------------------------------------
-// Copyright © 2010-2020 Matthias Kläy.
+// Copyright © 2003-2004 HSR Hochschule für Technik Rapperswil.
+// Copyright © 2003-2020 Matthias Kläy.
 // All rights reserved.
 // ------------------------------------------------------------------------------------------------
 // This source code is licensed under the GNU LGPL.
 // See http://www.gnu.org/licenses/lgpl.html for license details.
 //==================================================================================================
 
-using System.Diagnostics.CodeAnalysis;
-
-using NUnit.Framework;
-
-namespace NUnit
+namespace NUnitEx
 {
-	/// <summary></summary>
-	[TestFixture]
-	public class ContextDetectorTest
+	/// <summary>
+	/// Encapsulates information to initialize <see cref="Framework.TestCaseData"/> objects.
+	/// </summary>
+	/// <remarks>
+	/// Useful to encapsulate information where <see cref="Framework.TestCaseData"/> objects are
+	/// not suitable or suboptimal, e.g. where named and typed arguments shall be used rather than
+	/// just an array of <see cref="object"/> named "arg1", "arg2",...
+	/// </remarks>
+	/// <remarks>
+	/// Simply derive from this class and add arguments as needed.
+	/// </remarks>
+	/// <remarks>
+	/// Name "TestCaseDescriptor" rather than e.g. "TestCaseDataHelper" for better distinguishing
+	/// from <see cref="Framework.TestCaseData"/>.
+	/// </remarks>
+	public class TestCaseDescriptor
 	{
-		/// <summary></summary>
-		[SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly", MessageId = "TestIs", Justification = "FxCop...")]
-		[Test]
-		public virtual void TestIsRunningInNUnit()
+		/// <summary>
+		/// The name for setting <see cref="Framework.TestCaseData.TestName"/>.
+		/// </summary>
+		public string Name { get; }
+
+		/// <summary>
+		/// The categories for setting <see cref="Framework.TestCaseData.Categories"/>.
+		/// </summary>
+		public string[] Categories { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TestCaseDescriptor"/> class.
+		/// </summary>
+		public TestCaseDescriptor(string name, string[] categories)
 		{
-			Assert.IsTrue(ContextDetector.IsRunningInNUnit);
+			Name = name;
+			Categories = categories;
 		}
 	}
 }
