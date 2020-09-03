258251d16f2cd59be9977b6bfd968e6ff3811c0e
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2020-09-03 18:58:56 +0000

    YAT 2.2.0 Beta Version 2.1.94

--- trunk/YAT/YAT.Version.cs
+++ trunk/YAT/YAT.Version.cs
@@ -35,9 +35,9 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")] => 'Build' will indicate number of days since 2000-01-01.
 //                                      => 'Revision' will indicate number of seconds since midnight divided by two.
-[assembly: AssemblyVersion("2.1.93.*")]
-[assembly: AssemblyFileVersion("2.1.93.0")]
-[assembly: AssemblyInformationalVersion("2.1.93")]
+[assembly: AssemblyVersion("2.1.94.*")]
+[assembly: AssemblyFileVersion("2.1.94.0")]
+[assembly: AssemblyInformationalVersion("2.1.94")]
 
 //==================================================================================================
 // End of
