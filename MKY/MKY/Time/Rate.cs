b24528d8e7eb8173c2e319a6fd1c3ea85ee8a605
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2021-01-22 20:29:47 +0000

    Rate calculation partly fixed, but doesn't fully work for odd ratios => trying a different approach

--- MKY/MKY/Time/Rate.cs
+++ MKY/MKY/Time/Rate.cs
@@ -51,19 +51,35 @@ using System.Globalization;
 
 namespace MKY.Time
 {
-	/// <summary></summary>
+	/// <remarks>
+	/// A weighted sum is used to calculate the <see cref="Value"/> for better seeing changes.
+	/// </remarks>
 	public class Rate
 	{
+		#region Constants
+		//==========================================================================================
+		// Constants
+		//==========================================================================================
+
+		/// <summary>
+		/// The default value for <see cref="Interval"/>.
+		/// </summary>
+		public const int IntervalDefault = 1000;
+
+		#endregion
+
 		#region Fields
 		//==========================================================================================
 		// Fields
 		//==========================================================================================
 
-		private double window;
 		private double interval;
+		private double window;
 
-		private int numberOfIntervals;
-		private int[] itemsPerInterval;
+		private int numberOfBins;   // The number of bins is always integral, even in case of an odd interval-window-ratio.
+		private double widthOfBins; // But the width will become less than the interval in case of an odd interval-window-ratio.
+		private double binScaling;
+		private int[] itemsPerBin;
 
 		private Queue<TimeStampItem<int>> queue;
 		private int value;
@@ -86,7 +102,7 @@ namespace MKY.Time
 		/// <see cref="Interval"/> and <see cref="Window"/> to a second.
 		/// </summary>
 		public Rate()
-			: this(1000)
+			: this(IntervalDefault)
 		{
 		}
 
@@ -98,15 +114,18 @@ namespace MKY.Time
 		}
 
 		/// <param name="interval">The interval to calculate the value of the rate.</param>
-		/// <param name="window"><see cref="Window"/>.</param>
+		/// <param name="window"><see cref="Window" />.</param>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// Thrown if 'window' is less than 'interval'.
+		/// </exception>
 		public Rate(double interval, double window)
 		{
 			if (window < interval)
-				throw (new ArgumentOutOfRangeException("window", window, MessageHelper.InvalidExecutionPreamble + "'Window' = '" + window + "' must be equal or larger than 'Interval' = '" + interval + "'!" + Environment.NewLine + Environment.NewLine + MessageHelper.SubmitBug));
+				throw (new ArgumentOutOfRangeException("window", window, MessageHelper.InvalidExecutionPreamble + "'window' = '" + window + "' must be equal or larger than 'interval' = '" + interval + "'!" + Environment.NewLine + Environment.NewLine + MessageHelper.SubmitBug));
 
 			this.interval = interval;
 			this.window   = window;
-			UpdateNumberOfIntervals();
+			UpdateBins();
 
 			this.queue = new Queue<TimeStampItem<int>>(); // No clue how many items to expect, the default behavior must be good enough.
 		}
@@ -121,9 +140,12 @@ namespace MKY.Time
 		/// <summary>
 		/// The interval to calculate the value of the rate, in milliseconds.
 		/// </summary>
-		/// <remarks>Set to 0.001 to get <see cref="Value"/> in items per microsecond.</remarks>
 		/// <remarks>Set to 1 to get <see cref="Value"/> in items per millisecond.</remarks>
 		/// <remarks>Set to 1000 to get <see cref="Value"/> in items per second.</remarks>
+		/// <remarks>Set to 60000 to get <see cref="Value"/> in items per minute.</remarks>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// Thrown if 'Interval' is greater than 'Window'.
+		/// </exception>
 		public virtual double Interval
 		{
 			get { return (this.interval); }
@@ -133,7 +155,7 @@ namespace MKY.Time
 					throw (new ArgumentOutOfRangeException("value", value, MessageHelper.InvalidExecutionPreamble + "'Interval' must be equal or less than 'Window' = '" + window + "'!" + Environment.NewLine + Environment.NewLine + MessageHelper.SubmitBug));
 
 				this.interval = value;
-				UpdateNumberOfIntervals();
+				UpdateBins();
 			}
 		}
 
@@ -142,9 +164,12 @@ namespace MKY.Time
 		/// </summary>
 		/// <remarks>
 		/// If window is larger than <see cref="Interval"/>, a value is calculated for each interval
-		/// and the values are weighed. The value of the less recent interval is weighed most, more
-		/// recent intervals are weighed less.
+		/// and the values are weighted. The value of the less recent interval is weighed most, more
+		/// recent intervals are weighted less.
 		/// </remarks>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// Thrown if 'Window' is less than 'Interval'.
+		/// </exception>
 		public virtual double Window
 		{
 			get { return (this.window); }
@@ -154,7 +179,7 @@ namespace MKY.Time
 					throw (new ArgumentOutOfRangeException("value", value, MessageHelper.InvalidExecutionPreamble + "'Window' must be equal or larger than 'Interval' = '" + interval + "'!" + Environment.NewLine + Environment.NewLine + MessageHelper.SubmitBug));
 
 				this.window = value;
-				UpdateNumberOfIntervals();
+				UpdateBins();
 			}
 		}
 
@@ -242,15 +267,22 @@ namespace MKY.Time
 
 			this.lastItemTimeStamp = now; // While there may be no need to add, still use this time stamp.
 
-			if (item != 0)
-				return (AddItemAndUpdate(now, item, out value));
-			else // There is no need to add zero-items to queue; improves performance especially in case of frequent updates.
-				return (UpdateWithoutAdd(now, out value));
+			if (item == 0) // There is no need to add zero-items to the queue; improves performance especially in case of frequent updates.
+				return (Update(now, out value));
+			else
+				return (Update(now, item, out value));
+		}
+
+		/// <summary></summary>
+		public virtual bool Update(DateTime timeStamp, int item)
+		{
+			int value;
+			return (Update(timeStamp, item, out value));
 		}
 
 		/// <summary></summary>
 		[SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "2#", Justification = "Multiple return values are required, and 'out' is preferred to 'ref'.")]
-		protected virtual bool AddItemAndUpdate(DateTime timeStamp, int item, out int value)
+		public virtual bool Update(DateTime timeStamp, int item, out int value)
 		{
 			AddItemToQueue(timeStamp, item);
 			RemoveObsoleteItemsFromQueue(timeStamp);
@@ -269,13 +301,6 @@ namespace MKY.Time
 		}
 
 		/// <summary></summary>
-		public virtual bool Update(DateTime timeStamp)
-		{
-			int value;
-			return (Update(timeStamp, out value));
-		}
-
-		/// <summary></summary>
 		[SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "0#", Justification = "Multiple return values are required, and 'out' is preferred to 'ref'.")]
 		[CLSCompliant(false)]
 		public virtual bool Update(out int value)
@@ -285,15 +310,16 @@ namespace MKY.Time
 		}
 
 		/// <summary></summary>
-		[SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "1#", Justification = "Multiple return values are required, and 'out' is preferred to 'ref'.")]
-		public virtual bool Update(DateTime timeStamp, out int value)
+		public virtual bool Update(DateTime timeStamp)
 		{
-			return (UpdateWithoutAdd(timeStamp, out value));
+			int value;
+			return (Update(timeStamp, out value));
 		}
 
 		/// <summary></summary>
 		[SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "1#", Justification = "Multiple return values are required, and 'out' is preferred to 'ref'.")]
-		protected virtual bool UpdateWithoutAdd(DateTime timeStamp, out int value)
+		[CLSCompliant(false)]
+		public virtual bool Update(DateTime timeStamp, out int value)
 		{
 			RemoveObsoleteItemsFromQueue(timeStamp);
 			var hasChanged = CalculateValueFromQueue(timeStamp, out value);
@@ -315,11 +341,13 @@ namespace MKY.Time
 		// Methods > Private
 		//------------------------------------------------------------------------------------------
 
-		private void UpdateNumberOfIntervals()
+		private void UpdateBins()
 		{
-			this.numberOfIntervals = (int)(Math.Ceiling(this.window / this.interval));
-			this.itemsPerInterval = ArrayEx.CreateAndInitializeInstance(this.numberOfIntervals, 0); // Single array instance for performance optimization.
-		}                                                                                           // Makes no sense to allocate and discard on each update.
+			this.numberOfBins = (int)(Math.Ceiling(this.window / this.interval));
+			this.widthOfBins  = (this.window / this.numberOfBins); // Bins must be equally distributed across window.
+			this.binScaling   = (this.interval / this.widthOfBins); // Value must be scaled for odd-interval-window ratios.
+			this.itemsPerBin  = ArrayEx.CreateAndInitializeInstance(this.numberOfBins, 0); // Single array instance for performance optimization.
+		}                                                                                  // Makes no sense to allocate and discard on each update.
 
 		private void ClearQueue()
 		{
@@ -342,8 +370,8 @@ namespace MKY.Time
 				while (this.queue.Count > 0)
 				{
 					var tsi = this.queue.Peek();
-					if (tsi.TimeStamp < beginningOfWindow)
-						this.queue.Dequeue();
+					if (tsi.TimeStamp <= beginningOfWindow) // Window must be ]window], i.e. excluding the lower limit.
+						this.queue.Dequeue();               // Otherwise, the lowest bin typically contains a supernumerous item.
 					else
 						break; // Front-most item is within window.
 				}
@@ -358,32 +386,52 @@ namespace MKY.Time
 				qa = this.queue.ToArray();
 
 			// Prepare calculation:
-			Array.Clear(this.itemsPerInterval, 0, this.itemsPerInterval.Length);
+			Array.Clear(this.itemsPerBin, 0, this.itemsPerBin.Length);
 
 			unchecked
 			{
-				// Count number of items within each interval:
+				// Count number of items within each interval bin, starting with the most recent item:
 				foreach (TimeStampItem<int> tsi in qa) // Queue will only contain items within the window.
 				{
+					// Example: Interval = 1000, Window = 4000, UpdateInterval = 500, Items = { 1 }
+					//
+					// Window:
+					// [     |     |     |     ]
+					// 1  1  1  1  1  1  1  1  1
+					//       ^ this item results in ts.TotalMilliseconds = 300 and must be also placed into most recent bin
+					//    ^ this item results in ts.TotalMilliseconds = 3500 and must be placed into most recent bin
+					// ^ this item got dequeued as it is <= window
+					//
+					// Intervals:
+					// ]     ]     ]     ]     ]
+					//    1  1  1  1  1  1  1  1
+					//        ^ 2999 must result in index 2
+					//       ^ 3000 must result in index 3
+					//
+					// Bins:
+					// |  3  |  2  |  1  |  0  |
+
 					TimeSpan ts = (timeStamp - tsi.TimeStamp);
-					int i = Int32Ex.Limit((int)(Math.Round(ts.TotalMilliseconds / this.interval)), 0, (this.itemsPerInterval.Length - 1));
-					this.itemsPerInterval[i] += tsi.Item;
+					int index = (int)(Math.Floor(ts.TotalMilliseconds / this.widthOfBins));
+					int indexSafe = Int32Ex.Limit(index, 0, (this.itemsPerBin.Length - 1));
+					this.itemsPerBin[indexSafe] += tsi.Item;
 				}
 
-				// Weigh and sum up the intervals:
-				int weight = this.numberOfIntervals;
-				int weighedSum = 0;
+				// Weigh and sum up the interval bins:
+				int weight = this.numberOfBins;
+				double weightedSum = 0;
 				int sumOfWeights = 0;
-				foreach (int itemsOfInterval in this.itemsPerInterval)
+				foreach (int items in this.itemsPerBin)
 				{
-					weighedSum += (itemsOfInterval * weight);
+					weightedSum += (this.binScaling * items * weight);
 					sumOfWeights += weight;
 					weight--;
 				}
 
 				// Evaluate the rate:
-				value = (int)(Math.Round((double)weighedSum / sumOfWeights));
-			}
+				var valueUnrounded = (weightedSum / sumOfWeights);
+				value = (int)(Math.Round(valueUnrounded));
+			} // unchecked
 
 			if (this.value != value)
 			{
