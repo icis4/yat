d03e3eb66b8a2e43a08fc971c340e465dae5c174
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2021-02-01 16:53:14 +0000

    YAT 2.4.0 integrated into scripting

--- trunk/YAT/YAT.View.Forms/Help.cs
+++ trunk/YAT/YAT.View.Forms/Help.cs
@@ -40,6 +40,9 @@ namespace YAT.View.Forms
 
 			// Form:
 			Text = ApplicationEx.CommonName + " Help"; // Fixed to "YAT".
+		#if (WITH_SCRIPTING)
+			Icon = Properties.Resources.Icon_Albatros;
+		#endif
 
 			// Text:
 			textBox_ParserFormat.Text        = Domain.Parser.Parser.FormatHelp;
