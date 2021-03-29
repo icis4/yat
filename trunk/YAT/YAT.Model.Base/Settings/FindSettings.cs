36bbb1903f9545d299f0a373cecb0b8a45bd7e01
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2021-03-29 15:03:30 +0000

    Find text can now automatically be set by either selecting a line in a monitor or by selecting the whole or parts of a line in [Copy of Active Line] and [Find All] further prepared (related to feature requests #424 and #434)

---
+++ trunk/YAT/YAT.Model.Base/Settings/FindSettings.cs
@@ -0,0 +1,174 @@
+﻿//==================================================================================================
+// YAT - Yet Another Terminal.
+// Visit YAT at https://sourceforge.net/projects/y-a-terminal/.
+// Contact YAT by mailto:y-a-terminal@users.sourceforge.net.
+// ------------------------------------------------------------------------------------------------
+// $URL$
+// $Revision$
+// $Date$
+// $Author$
+// ------------------------------------------------------------------------------------------------
+// YAT Version 2.4.0
+// ------------------------------------------------------------------------------------------------
+// See release notes for product version details.
+// See SVN change log for file revision details.
+// Author(s): Matthias Klaey
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2003-2004 HSR Hochschule für Technik Rapperswil.
+// Copyright © 2003-2021 Matthias Kläy.
+// All rights reserved.
+// ------------------------------------------------------------------------------------------------
+// This source code is licensed under the GNU LGPL.
+// See http://www.gnu.org/licenses/lgpl.html for license details.
+//==================================================================================================
+
+using System;
+using System.Xml.Serialization;
+
+namespace YAT.Model.Settings
+{
+	/// <summary></summary>
+	public class FindSettings : MKY.Settings.SettingsItem, IEquatable<FindSettings>
+	{
+		private bool allIsActive;
+
+		/// <summary></summary>
+		public FindSettings()
+			: this(MKY.Settings.SettingsType.Explicit)
+		{
+		}
+
+		/// <summary></summary>
+		public FindSettings(MKY.Settings.SettingsType settingsType)
+			: base(settingsType)
+		{
+			SetMyDefaults();
+			ClearChanged();
+		}
+
+		/// <remarks>
+		/// Fields are assigned via properties even though changed flag will be cleared anyway.
+		/// There potentially is additional code that needs to be run within the property method.
+		/// </remarks>
+		public FindSettings(FindSettings rhs)
+			: base(rhs)
+		{
+			AllIsActive = rhs.AllIsActive;
+
+			ClearChanged();
+		}
+
+		/// <remarks>
+		/// Fields are assigned via properties to ensure correct setting of changed flag.
+		/// </remarks>
+		protected override void SetMyDefaults()
+		{
+			base.SetMyDefaults();
+
+			AllIsActive = false;
+		}
+
+		#region Properties
+		//==========================================================================================
+		// Properties
+		//==========================================================================================
+
+		/// <summary></summary>
+		[XmlElement("AllIsActive")]
+		public virtual bool AllIsActive
+		{
+			get { return (this.allIsActive); }
+			set
+			{
+				if (this.allIsActive != value)
+				{
+					this.allIsActive = value;
+					SetMyChanged();
+				}
+			}
+		}
+
+		#endregion
+
+		#region Object Members
+		//==========================================================================================
+		// Object Members
+		//==========================================================================================
+
+		/// <summary>
+		/// Serves as a hash function for a particular type.
+		/// </summary>
+		/// <remarks>
+		/// Use properties instead of fields to calculate hash code. This ensures that 'intelligent'
+		/// properties, i.e. properties with some logic, are also properly handled.
+		/// </remarks>
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				int hashCode = base.GetHashCode(); // Get hash code of all settings nodes.
+
+				hashCode = (hashCode * 397) ^ AllIsActive.GetHashCode();
+
+				return (hashCode);
+			}
+		}
+
+		/// <summary>
+		/// Determines whether this instance and the specified object have reference or value equality.
+		/// </summary>
+		public override bool Equals(object obj)
+		{
+			return (Equals(obj as FindSettings));
+		}
+
+		/// <summary>
+		/// Determines whether this instance and the specified object have reference or value equality.
+		/// </summary>
+		/// <remarks>
+		/// Use properties instead of fields to determine equality. This ensures that 'intelligent'
+		/// properties, i.e. properties with some logic, are also properly handled.
+		/// </remarks>
+		public bool Equals(FindSettings other)
+		{
+			if (ReferenceEquals(other, null)) return (false);
+			if (ReferenceEquals(this, other)) return (true);
+			if (GetType() != other.GetType()) return (false);
+
+			return
+			(
+				base.Equals(other) && // Compare all settings nodes.
+
+				AllIsActive.Equals(other.AllIsActive)
+			);
+		}
+
+		/// <summary>
+		/// Determines whether the two specified objects have reference or value equality.
+		/// </summary>
+		public static bool operator ==(FindSettings lhs, FindSettings rhs)
+		{
+			if (ReferenceEquals(lhs, rhs))  return (true);
+			if (ReferenceEquals(lhs, null)) return (false);
+			if (ReferenceEquals(rhs, null)) return (false);
+
+			object obj = (object)lhs; // Operators are not virtual! Calling object.Equals() ensures
+			return (obj.Equals(rhs)); // that a potential virtual <Derived>.Equals() is called.
+		}
+
+		/// <summary>
+		/// Determines whether the two specified objects have reference and value inequality.
+		/// </summary>
+		public static bool operator !=(FindSettings lhs, FindSettings rhs)
+		{
+			return (!(lhs == rhs));
+		}
+
+		#endregion
+	}
+}
+
+//==================================================================================================
+// End of
+// $URL$
+//==================================================================================================
