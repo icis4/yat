813c05f322b3cc4f152e12bdd521175068f26d8a
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2021-03-31 09:18:59 +0000

    Minor debug refinement

--- MKY/MKY/ExceptionHelper.cs
+++ MKY/MKY/ExceptionHelper.cs
@@ -146,7 +146,7 @@ namespace MKY
 				string.Format
 				(
 					CultureInfo.CurrentCulture,
-					" @ {0} @ Thread #{1} : {2,36} {3,3} {4,-38} : {5}",
+					" @ {0} @ Thread#{1} : {2,36} {3,3} {4,-38} : {5}",
 					DateTime.Now.ToString("HH:mm:ss.fff", DateTimeFormatInfo.CurrentInfo),
 					Thread.CurrentThread.ManagedThreadId.ToString("D3", CultureInfo.CurrentCulture),
 					GetType(),
