648c7e89d3e461418435b8ac3d01ff4bbe212272
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2021-03-31 09:24:21 +0000

    Minor debug refinement

--- trunk/ALAZ/Source/ALAZ.SystemEx.NetEx/SocketsEx/BaseSocketConnectionHost.cs
+++ trunk/ALAZ/Source/ALAZ.SystemEx.NetEx/SocketsEx/BaseSocketConnectionHost.cs
@@ -2772,7 +2772,7 @@ namespace ALAZ.SystemEx.NetEx.SocketsEx
 				string.Format
 				(
 					CultureInfo.CurrentCulture,
-					" @ {0} @ Thread#{1} : {2,36} {3,3} {4,-38} : {5}",
+					" @ {0} @ Thread #{1} : {2,36} {3,3} {4,-38} : {5}",
 					DateTime.Now.ToString("HH:mm:ss.fff", DateTimeFormatInfo.CurrentInfo),
 					Thread.CurrentThread.ManagedThreadId.ToString("D3", CultureInfo.CurrentCulture),
 					GetType(),
