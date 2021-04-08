beb858c4bcbfa04fff0beb8508e9c3a193520e4e
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2021-04-08 15:05:18 +0000

    CallerEx comment refinement

--- trunk/MKY/MKY/Runtime/CompilerServices/CallerEx.cs
+++ trunk/MKY/MKY/Runtime/CompilerServices/CallerEx.cs
@@ -34,14 +34,19 @@ namespace MKY.Runtime.CompilerServices
 	[SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix", Justification = "'Ex' emphasizes that it's an extension to an existing class and not a replacement as '2' would emphasize.")]
 	public static class CallerEx
 	{
-		/// <remarks><para>
-		/// Considering performance as described in
+		/// <summary>
+		/// Convenience method that allows retrieving caller information by a caller itself.
+		/// </summary>
+		/// <remarks>
+		/// Based on <see cref="CallerMemberNameAttribute"/>.
+		/// <para>
+		/// Considering performance as described at
 		/// https://stackoverflow.com/questions/1348643/how-performant-is-stackframe
 		/// and
 		/// https://stackoverflow.com/questions/16101152/is-performance-hit-by-using-caller-information-attributes
 		/// calling this method is preferred over using reflection.
 		/// <para></para>
-		/// Considering maintenance and error-proneness calling this method is preferred over using
+		/// Considering maintenance and error-proneness, calling this method is preferred over using
 		/// the <code>nameof()</code> operator, as that operator requires to provide the very method
 		/// name, whereas with this method the method does not need any identifier.
 		/// </para></remarks>
@@ -50,14 +55,24 @@ namespace MKY.Runtime.CompilerServices
 			return (callerMemberName);
 		}
 
-		/// <summary></summary>
-		public static string GetCallerFilePath(string callerFilePath = "")
+		/// <summary>
+		/// Convenience method that allows retrieving caller information by a caller itself.
+		/// </summary>
+		/// <remarks>
+		/// Based on <see cref="CallerFilePathAttribute"/>.
+		/// </remarks>
+		public static string GetCallerFilePath([CallerFilePath] string callerFilePath = "")
 		{
 			return (callerFilePath);
 		}
 
-		/// <summary></summary>
-		public static int GetCallerLineNumber(int callerLineNumber = 0)
+		/// <summary>
+		/// Convenience method that allows retrieving caller information by a caller itself.
+		/// </summary>
+		/// <remarks>
+		/// Based on <see cref="CallerLineNumberAttribute"/>.
+		/// </remarks>
+		public static int GetCallerLineNumber([CallerLineNumber] int callerLineNumber = 0)
 		{
 			return (callerLineNumber);
 		}
