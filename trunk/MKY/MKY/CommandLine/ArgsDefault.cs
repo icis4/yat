8f0ea37b71cb2235ffacbabd18bd235c35e11299
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2021-04-28 08:25:10 +0000

    Minor command line related improvement in MKY.CommandLine (required for fix of bug #503)

---
+++ trunk/MKY/MKY/CommandLine/ArgsDefault.cs
@@ -0,0 +1,44 @@
+﻿//==================================================================================================
+// YAT - Yet Another Terminal.
+// Visit YAT at https://sourceforge.net/projects/y-a-terminal/.
+// Contact YAT by mailto:y-a-terminal@users.sourceforge.net.
+// ------------------------------------------------------------------------------------------------
+// $URL$
+// $Revision$
+// $Date$
+// $Author$
+// ------------------------------------------------------------------------------------------------
+// MKY Version 1.0.30
+// ------------------------------------------------------------------------------------------------
+// See release notes for product version details.
+// See SVN change log for file revision details.
+// Author(s): Matthias Klaey
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2010-2021 Matthias Kläy.
+// All rights reserved.
+// ------------------------------------------------------------------------------------------------
+// This source code is licensed under the GNU LGPL.
+// See http://www.gnu.org/licenses/lgpl.html for license details.
+//==================================================================================================
+
+using System;
+
+namespace MKY.CommandLine
+{
+	/// <summary>
+	/// Helper class.
+	/// </summary>
+	public static class ArgsDefault
+	{
+		/// <summary>
+		/// The default value of a C# <code>main(string[])</code> is an
+		/// empty <see cref="string"/> <see cref="Array"/>, not <c>null</c>.
+		/// </summary>
+		public static readonly string[] Empty = new string[] { };
+	}
+}
+
+//==================================================================================================
+// End of
+// $URL$
+//==================================================================================================
