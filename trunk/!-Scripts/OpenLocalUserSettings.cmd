9b8e9595e3477d28e693d2d6c57d5382b94f7789
maettu_this <maettu_this@57fe84ae-c09f-4183-8c35-32342bd6d1e5>
2021-05-19 12:27:06 +0000

    Helper script to open local/roaming user settings added

---
+++ trunk/!-Scripts/OpenLocalUserSettings.cmd
@@ -0,0 +1,39 @@
+@ECHO OFF
+
+::==================================================================================================
+:: YAT - Yet Another Terminal.
+:: Visit YAT at https://sourceforge.net/projects/y-a-terminal/.
+:: Contact YAT by mailto:y-a-terminal@users.sourceforge.net.
+:: ------------------------------------------------------------------------------------------------
+:: $URL: https://svn.code.sf.net/p/y-a-terminal/code/trunk/!-Scripts/CallPowerShellScript.cmd $
+:: $Revision: 3584 $
+:: $Date: 2021-01-04 16:40:12 +0100 (Mo., 04 Jan 2021) $
+:: $Author: maettu_this $
+:: ------------------------------------------------------------------------------------------------
+:: See release notes for product version details.
+:: See SVN change log for file revision details.
+::
+::     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+::     Attention: Windows/DOS requires that this file is encoded in ASCII/ANSI and not UTF-8!
+::     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+::
+:: Author(s): Matthias Klaey
+:: ------------------------------------------------------------------------------------------------
+:: Copyright © 2003-2021 Matthias Kläy.
+:: All rights reserved.
+:: ------------------------------------------------------------------------------------------------
+:: This source code is licensed under the GNU LGPL.
+:: See http://www.gnu.org/licenses/lgpl.html for license details.
+::==================================================================================================
+
+SETLOCAL
+
+SET "_path=%USERPROFILE%\AppData\Local\YAT"
+START "" "%_path%"
+
+ENDLOCAL
+
+::==================================================================================================
+:: End of
+:: $URL: https://svn.code.sf.net/p/y-a-terminal/code/trunk/!-Scripts/CallPowerShellScript.cmd $
+::==================================================================================================
